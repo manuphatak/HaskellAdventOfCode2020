e4328ceed24236b9939262434dce84c0cf1eb9b2
Manu Phatak <manu.phatak@procore.com>
2020-11-30 23:56:06 -0800

    Solve part 1

--- src/Day01/Solution.hs
+++ src/Day01/Solution.hs
@@ -1,7 +1,38 @@
-module Day01.Solution (part1, part2) where
+module Day01.Solution (part1, part2, goalSeek) where
+
+import Control.Monad ((<=<))
+import Data.Foldable (find)
+import Data.List (tails)
+import Data.Maybe (fromJust)
 
 part1 :: String -> String
-part1 = id
+part1 = show . fromJust . goalSeek 2020 . map readInt . lines
 
 part2 :: String -> String
 part2 = id
+
+readInt :: String -> Int
+readInt n = read n :: Int
+
+goalSeek :: Int -> [Int] -> Maybe Int
+goalSeek target = uncurry maybeProduct <=< find (uncurry sumEqualGoals) . pairs
+  where
+    sumEqualGoals :: Int -> Int -> Bool
+    sumEqualGoals l r = l + r == target
+    maybeProduct :: Int -> Int -> Maybe Int
+    maybeProduct a b = Just (a * b)
+
+pairs :: [a] -> [(a, a)]
+pairs = map pair . combinations 2
+  where
+    pair :: [b] -> (b, b)
+    pair [x, y] = (x, y)
+    pair _ = error "this should never happen"
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n xs =
+  [ y : ys
+    | y : xs' <- tails xs,
+      ys <- combinations (n -1) xs'
+  ]
--- test/Day01/SolutionSpec.hs
+++ test/Day01/SolutionSpec.hs
@@ -1,13 +1,18 @@
 module Day01.SolutionSpec (spec) where
 
-import Day01.Solution (part1, part2)
+import Day01.Solution (goalSeek, part1, part2)
 import Test.Hspec
 
 spec :: Spec
 spec = do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day01/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "299299"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day01/input.txt"
     part2 input `shouldBe` "hello santa"
+  context "given a list of numbers" $
+    let needle = 2020
+        haystack = [1721, 979, 366, 299, 675, 1456]
+     in it "finds the product of the two numbers that add up to a target" $
+          goalSeek needle haystack `shouldBe` Just (1721 * 299)
