c801a75a368a269b5bfbd005e10472dd273ff885
Manu Phatak <bionikspoon@gmail.com>
2020-12-01 06:51:28 -0800

    Cleanup (#3)
    - use fmap instead of k-arrow

--- src/Day01/Solution.hs
+++ src/Day01/Solution.hs
@@ -1,8 +1,6 @@
 module Day01.Solution (part1, part2, goalSeek) where
 
-import Control.Monad ((<=<))
-import Data.Foldable (Foldable (foldl'), find)
-import Data.List (tails)
+import Data.List (find, tails)
 import Data.Maybe (fromJust)
 
 part1 :: String -> String
@@ -15,8 +13,8 @@ readInt :: String -> Int
 readInt n = read n :: Int
 
 goalSeek :: Int -> Int -> [Int] -> Maybe Int
-goalSeek n target = Just . foldl' (*) 1 <=< find ((target ==) . sum) . combinations n
+goalSeek n target = fmap product . find ((target ==) . sum) . combinations n
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
-combinations n xs = [y : ys | y : xs' <- tails xs, ys <- combinations (n -1) xs']
+combinations n xs = [y : ys | y : xs' <- tails xs, ys <- combinations (pred n) xs']
