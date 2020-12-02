62211a639a8619c20864aa13af8df8693adb5418
Manu Phatak <manu.phatak@procore.com>
2020-12-02 02:59:48 -0800

    Solve part 1

--- src/Day02/Solution.hs
+++ src/Day02/Solution.hs
@@ -1,7 +1,46 @@
-module Day02.Solution (part1, part2) where
+module Day02.Solution (part1, part2, PasswordPolicy (..), parseLine, isValid) where
+
+import Data.Maybe (fromJust)
+import Text.Parsec
 
 part1 :: String -> String
-part1 = id
+part1 = show . countValid . fromJust . mapM lineIsValid . lines
+  where
+    lineIsValid :: String -> Maybe Bool
+    lineIsValid = fmap (uncurry isValid) . parseLine
+    countValid :: [Bool] -> Int
+    countValid = length . filter (== True)
 
 part2 :: String -> String
 part2 = id
+
+type Password = String
+
+data PasswordPolicy = PasswordPolicy Int Int Char deriving (Show, Eq)
+
+parseLine :: String -> Maybe (PasswordPolicy, String)
+parseLine = rightToMaybe . parse lineParser ""
+  where
+    lineParser :: Parsec String st (PasswordPolicy, Password)
+    lineParser = (,) <$> passwordPolicyParser <*> (string ": " *> passwordParser <* eof)
+
+passwordPolicyParser :: Parsec String st PasswordPolicy
+passwordPolicyParser = PasswordPolicy <$> intParser <*> (char '-' *> intParser) <*> (space *> letter)
+
+intParser :: Parsec String st Int
+intParser = read <$> many1 digit
+
+passwordParser :: Parsec String st Password
+passwordParser = many1 letter
+
+isValid :: PasswordPolicy -> Password -> Bool
+isValid (PasswordPolicy l u c) password = isBetween l u $ occurrences c password
+
+occurrences :: Eq a => a -> [a] -> Int
+occurrences target = length . filter (target ==)
+
+isBetween :: Ord a => a -> a -> a -> Bool
+isBetween l u target = target >= l && target <= u
+
+rightToMaybe :: Either a b -> Maybe b
+rightToMaybe = either (const Nothing) Just
--- test/Day02/SolutionSpec.hs
+++ test/Day02/SolutionSpec.hs
@@ -1,13 +1,32 @@
 module Day02.SolutionSpec (spec) where
 
-import Day02.Solution (part1, part2)
+import Data.Foldable (for_)
+import Day02.Solution (PasswordPolicy (..), isValid, parseLine, part1, part2)
 import Test.Hspec
 
 spec :: Spec
 spec = do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day02/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "378"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day02/input.txt"
     part2 input `shouldBe` "hello santa"
+  describe "parseLine" $ do
+    let cases =
+          [ ("1-3 a: abcde", Just (PasswordPolicy 1 3 'a', "abcde")),
+            ("1-3 b: cdefg", Just (PasswordPolicy 1 3 'b', "cdefg")),
+            ("2-9 c: ccccccccc", Just (PasswordPolicy 2 9 'c', "ccccccccc"))
+          ]
+        test (input, expected) = it ("parses given a line " ++ input) $ do
+          parseLine input `shouldBe` expected
+     in for_ cases test
+  describe "isValid" $ do
+    let cases =
+          [ (PasswordPolicy 1 3 'a', "abcde", True),
+            (PasswordPolicy 1 3 'b', "cdefg", False),
+            (PasswordPolicy 2 9 'c', "ccccccccc", True)
+          ]
+        test (policy, password, expected) = context ("given a policy " ++ (show policy) ++ " and a password " ++ password) $ do
+          it ("validates as " ++ show expected) $ isValid policy password `shouldBe` expected
+     in for_ cases test
