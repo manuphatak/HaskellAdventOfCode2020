2762f3e4a83df598bac1c514872f1dbc1cb4965f
Manu Phatak <manu.phatak@procore.com>
2020-12-02 22:38:24 -0800

    Solve part 1

--- src/Day03/Solution.hs
+++ src/Day03/Solution.hs
@@ -1,7 +1,53 @@
-module Day03.Solution (part1, part2) where
+module Day03.Solution (part1, part2, followSlope) where
 
 part1 :: String -> String
-part1 = id
+part1 = followSlope 3 1
 
 part2 :: String -> String
 part2 = id
+
+data Cell = T | E deriving (Eq, Show)
+
+type Row = [Cell]
+
+type Grid = [Row]
+
+data Coordinates = Coordinates Int Int deriving (Show)
+
+type TreeCount = Int
+
+data Projection = Projection TreeCount Coordinates Grid
+
+instance Show Projection where
+  show (Projection treeCount coordinates _) = "(Projection " ++ show treeCount ++ " (" ++ show coordinates ++ ") Grid)"
+
+followSlope :: Int -> Int -> String -> String
+followSlope slopeX slopY = show . projectionTreeCount . walk slopeX slopY . asProjection . map (map readCell) . lines
+
+projectionTreeCount :: Projection -> TreeCount
+projectionTreeCount (Projection treeCount _ _) = treeCount
+
+walk :: Int -> Int -> Projection -> Projection
+walk slopeX slopeY projection@(Projection _ (Coordinates _ y) grid)
+  | y == (length grid - 1) = projection
+  | otherwise = walk slopeX slopeY (step slopeX slopeY projection)
+
+step :: Int -> Int -> Projection -> Projection
+step slopeX slopeY (Projection treeCount (Coordinates x y) grid) =
+  let x' = x + slopeX
+      y' = y + slopeY
+      cell = (grid !! y') !! x'
+      nextTreeCount = if cell == T then succ treeCount else treeCount
+   in Projection nextTreeCount (Coordinates x' y') grid
+
+asProjection :: Grid -> Projection
+asProjection grid = Projection 0 (Coordinates 0 0) (map cycle grid)
+
+readCell :: Char -> Cell
+readCell '.' = E
+readCell '#' = T
+readCell _ = error "unknown Cell"
+
+-- >>> input <- readFile "./test/Day03/example.txt"
+-- >>> fn input
+-- (Projection 7 (Coordinates 30 10) Grid)
--- test/Day03/SolutionSpec.hs
+++ test/Day03/SolutionSpec.hs
@@ -1,13 +1,19 @@
 module Day03.SolutionSpec (spec) where
 
-import Day03.Solution (part1, part2)
+import Day03.Solution (followSlope, part1, part2)
 import Test.Hspec
 
 spec :: Spec
 spec = do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day03/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "254"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day03/input.txt"
     part2 input `shouldBe` "hello santa"
+  describe "followSlope" $
+    context "given the example input" $ do
+      it "should count the number of trees" $ do
+        input <- readFile "./test/Day03/example.txt"
+
+        followSlope 3 1 input `shouldBe` "7"
