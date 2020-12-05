a7b9cc40905d8f213b4fc8e182cc187c4f9d95f7
Manu Phatak <manu.phatak@procore.com>
2020-12-04 22:28:27 -0800

    Solve part 1

--- src/Day05/Solution.hs
+++ src/Day05/Solution.hs
@@ -1,7 +1,32 @@
-module Day05.Solution (part1, part2) where
+module Day05.Solution (part1, part2, Seat (..), decode) where
 
-part1 :: String -> String
-part1 = id
+-- part1 :: String -> String
+part1 = show . maximum . map (seatId . decode) . lines
 
 part2 :: String -> String
 part2 = id
+
+data Seat = Seat {seatRow :: Int, seatCol :: Int, seatId :: Int} deriving (Show, Eq)
+
+decode :: String -> Seat
+decode = asSeat . decode' . splitAt 7
+  where
+    decode' :: (String, String) -> (Int, Int)
+    decode' (row, col) = (decodeRow row, decodeCol col)
+    asSeat :: (Int, Int) -> Seat
+    asSeat (row, col) = Seat {seatRow = row, seatCol = col, seatId = row * 8 + col}
+    decodeRow :: String -> Int
+    decodeRow [] = 0
+    decodeRow (x : xs)
+      | x == 'B' = 1 * (2 ^ length xs) + decodeRow xs
+      | otherwise = decodeRow xs
+
+    decodeCol :: String -> Int
+    decodeCol [] = 0
+    decodeCol (x : xs)
+      | x == 'R' = 1 * (2 ^ length xs) + decodeCol xs
+      | otherwise = decodeCol xs
+
+-- >>> input <- readFile "./test/Day05/input.txt"
+-- >>> part1 input
+-- 998
--- test/Day05/SolutionSpec.hs
+++ test/Day05/SolutionSpec.hs
@@ -1,13 +1,24 @@
 module Day05.SolutionSpec (spec) where
 
-import Day05.Solution (part1, part2)
+import Data.Foldable (for_)
+import Day05.Solution (Seat (..), decode, part1, part2)
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day05/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "998"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day05/input.txt"
     part2 input `shouldBe` "hello santa"
+  describe "decode" $ do
+    let cases =
+          [ ("BFFFBBFRRR", Seat {seatRow = 70, seatCol = 7, seatId = 567}),
+            ("FFFBBBFRRR", Seat {seatRow = 14, seatCol = 7, seatId = 119}),
+            ("BBFFBBFRLL", Seat {seatRow = 102, seatCol = 4, seatId = 820})
+          ]
+        test (input, expected) =
+          it ("converts " ++ input ++ " into " ++ show expected) $
+            decode input `shouldBe` expected
+     in for_ cases test
