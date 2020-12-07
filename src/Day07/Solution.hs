c8c82b13e485c82c3dbecf1a696a1f68145fdc44
Manu Phatak <manu.phatak@procore.com>
2020-12-07 08:32:55 -0800

    Build parser for part 1

--- src/Day07/Solution.hs
+++ src/Day07/Solution.hs
@@ -1,7 +1,46 @@
-module Day07.Solution (part1, part2) where
+module Day07.Solution (part1, part2, parseRules, Rule (..)) where
+
+import Advent.Utils (readInt)
+import Text.Parsec
 
 part1 :: String -> String
 part1 = head . lines
 
 part2 :: String -> String
 part2 = head . lines
+
+type Bag = String
+
+data Rule = Rule Bag [(Int, Bag)] deriving (Show, Eq)
+
+parseRules :: String -> Either ParseError [Rule]
+parseRules = parse (try ruleParser `sepEndBy1` newline) ""
+
+ruleParser :: Parsec String () Rule
+ruleParser =
+  (Rule <$> (bagParser <* string " bags contain") <*> choice [try containsNoBagsParser, containsBagsParser]) <* char '.'
+
+containsNoBagsParser :: Parsec String () [(Int, Bag)]
+containsNoBagsParser = do
+  _ <- string " no other bags"
+  pure []
+
+containsBagsParser :: Parsec String () [(Int, Bag)]
+containsBagsParser = bagCountParser `sepBy1` char ','
+
+bagCountParser :: Parsec String () (Int, Bag)
+bagCountParser = (,) <$> countParser <*> bagParser'
+  where
+    countParser = space *> (readInt <$> many digit) <* space
+    bagParser' = bagParser <* space <* skipMany1 letter
+
+bagParser :: Parsec String () Bag
+bagParser =
+  manyTill anyChar $
+    try $
+      lookAhead $
+        string " bag"
+
+-- >>> input <- readFile "./test/Day07/example.txt"
+-- >>> parseRules input
+-- Right [Rule "light red" [(1,"bright white"),(2,"muted yellow")],Rule "dark orange" [(3,"bright white"),(4,"muted yellow")],Rule "bright white" [(1,"shiny gold")],Rule "muted yellow" [(2,"shiny gold"),(9,"faded blue")],Rule "shiny gold" [(1,"dark olive"),(2,"vibrant plum")],Rule "dark olive" [(3,"faded blue"),(4,"dotted black")],Rule "vibrant plum" [(5,"faded blue"),(6,"dotted black")],Rule "faded blue" [],Rule "dotted black" []]
--- test/Day07/SolutionSpec.hs
+++ test/Day07/SolutionSpec.hs
@@ -1,6 +1,6 @@
 module Day07.SolutionSpec (spec) where
 
-import Day07.Solution (part1, part2)
+import Day07.Solution (Rule (..), parseRules, part1, part2)
 import Test.Hspec
 
 spec :: Spec
@@ -11,3 +11,20 @@ spec = parallel $ do
   xit "solves Part 2" $ do
     input <- readFile "./test/Day07/input.txt"
     part2 input `shouldBe` "hello santa"
+  describe "parseRules" $
+    context "given the file example.txt" $
+      it "parses the rules" $ do
+        input <- readFile "./test/Day07/example.txt"
+        let expected =
+              Right
+                [ Rule "light red" [(1, "bright white"), (2, "muted yellow")],
+                  Rule "dark orange" [(3, "bright white"), (4, "muted yellow")],
+                  Rule "bright white" [(1, "shiny gold")],
+                  Rule "muted yellow" [(2, "shiny gold"), (9, "faded blue")],
+                  Rule "shiny gold" [(1, "dark olive"), (2, "vibrant plum")],
+                  Rule "dark olive" [(3, "faded blue"), (4, "dotted black")],
+                  Rule "vibrant plum" [(5, "faded blue"), (6, "dotted black")],
+                  Rule "faded blue" [],
+                  Rule "dotted black" []
+                ]
+        parseRules input `shouldBe` expected
