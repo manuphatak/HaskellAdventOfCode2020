0f7c505f56126c9ac3f82f5d17980d8c26b04fe6
Manu Phatak <manu.phatak@procore.com>
2020-12-07 21:09:41 -0800

    wip

--- src/Day07/Solution.hs
+++ src/Day07/Solution.hs
@@ -1,6 +1,8 @@
-module Day07.Solution (part1, part2, parseRules, Rule (..)) where
+module Day07.Solution (part1, part2, parseRules, Rules, pathsToTarget, Tree (..), asTree, expand) where
 
 import Advent.Utils (readInt)
+import Control.Monad (join)
+import qualified Data.Map.Strict as Map
 import Text.Parsec
 
 part1 :: String -> String
@@ -11,14 +13,14 @@ part2 = head . lines
 
 type Bag = String
 
-data Rule = Rule Bag [(Int, Bag)] deriving (Show, Eq)
+type Rules = Map.Map Bag [(Int, Bag)]
 
-parseRules :: String -> Either ParseError [Rule]
-parseRules = parse (try ruleParser `sepEndBy1` newline) ""
+parseRules :: String -> Either ParseError Rules
+parseRules = parse (Map.fromList <$> try ruleParser `sepEndBy1` newline) ""
 
-ruleParser :: Parsec String () Rule
+ruleParser :: Parsec String () (Bag, [(Int, Bag)])
 ruleParser =
-  (Rule <$> (bagParser <* string " bags contain") <*> choice [try containsNoBagsParser, containsBagsParser]) <* char '.'
+  ((,) <$> (bagParser <* string " bags contain") <*> choice [try containsNoBagsParser, containsBagsParser]) <* char '.'
 
 containsNoBagsParser :: Parsec String () [(Int, Bag)]
 containsNoBagsParser = do
@@ -41,6 +43,49 @@ bagParser =
       lookAhead $
         string " bag"
 
+data Tree = Leaf (Int, Bag) | Tree (Int, Bag) (Maybe [Tree]) deriving (Show, Eq)
+
+pathsToTarget :: Bag -> Rules -> Map.Map Bag (Maybe [[(Int, Bag)]])
+pathsToTarget target = Map.map go
+  where
+    go :: [(Int, Bag)] -> Maybe [[(Int, Bag)]]
+    go x = Just [x]
+
+asTree :: Bag -> Rules -> Map.Map Bag [Tree]
+asTree target rules = Map.map go rules
+  where
+    go :: [(Int, Bag)] -> [Tree]
+    go [] = []
+    go (x@(_, bag) : xs)
+      | bag == target = (Leaf x) : go xs
+      | otherwise = Tree x (go <$> (Map.lookup bag rules)) : (go xs)
+
+expand :: Tree -> [[(Int, Bag)]]
+expand _t = go ([], _t)
+  where
+    go :: ([[(Int, Bag)]], Tree) -> [[(Int, Bag)]]
+    go ([], Leaf x) = [[x]]
+    go (acc, Leaf x) = map ((:) x) acc
+    go (acc, Tree x (Just trees)) =
+      let expanded = (join $ map (\_ -> go (acc, Leaf x)) trees)
+       in (join $ map (\tree -> go (expanded, tree)) trees)
+
+-- expand (Tree x (Maybe trees)) =
+
+-- pathsToTarget target rules = map (pathToTarget target rules) $ Map.keys rules
+
+-- -- pathToTarget :: Bag -> Rules -> Bag -> [Maybe [(Int, Bag)]]
+-- pathToTarget target rules key = go [] key
+--   where
+--     -- go :: [(Int, Bag)] -> Bag -> Maybe [(Int, Bag)]
+--     go visited key
+--       | key == target = Just visited
+--       | otherwise = (\values -> (go (sequenceA (head values) : visited) key)) <$> Map.lookup target rules
+
+-- temp = (map (\(v@(_, nextKey)) -> go (v : visited) nextKey))
+
+-- [Maybe a] -> Maybe [a]
+
 -- >>> input <- readFile "./test/Day07/example.txt"
--- >>> parseRules input
--- Right [Rule "light red" [(1,"bright white"),(2,"muted yellow")],Rule "dark orange" [(3,"bright white"),(4,"muted yellow")],Rule "bright white" [(1,"shiny gold")],Rule "muted yellow" [(2,"shiny gold"),(9,"faded blue")],Rule "shiny gold" [(1,"dark olive"),(2,"vibrant plum")],Rule "dark olive" [(3,"faded blue"),(4,"dotted black")],Rule "vibrant plum" [(5,"faded blue"),(6,"dotted black")],Rule "faded blue" [],Rule "dotted black" []]
+-- >>> pathsToTarget "shiny gold" <$> (parseRules $ input)
+-- Right (fromList [("bright white",Just [[(1,"shiny gold")]]),("dark olive",Nothing),("dark orange",Nothing),("dotted black",Nothing),("faded blue",Nothing),("light red",Nothing),("muted yellow",Just [[(2,"shiny gold")]]),("shiny gold",Nothing),("vibrant plum",Nothing)])
--- test/Day07/SolutionSpec.hs
+++ test/Day07/SolutionSpec.hs
@@ -1,6 +1,8 @@
 module Day07.SolutionSpec (spec) where
 
-import Day07.Solution (Rule (..), parseRules, part1, part2)
+import Data.Foldable (for_)
+import qualified Data.Map.Strict as Map
+import Day07.Solution (Rules, Tree (..), asTree, expand, parseRules, part1, part2, pathsToTarget)
 import Test.Hspec
 
 spec :: Spec
@@ -11,20 +13,128 @@ spec = parallel $ do
   xit "solves Part 2" $ do
     input <- readFile "./test/Day07/input.txt"
     part2 input `shouldBe` "hello santa"
+
+  let parsedExample =
+        Map.fromList
+          [ ("light red", [(1, "bright white"), (2, "muted yellow")]),
+            ("dark orange", [(3, "bright white"), (4, "muted yellow")]),
+            ("bright white", [(1, "shiny gold")]),
+            ("muted yellow", [(2, "shiny gold"), (9, "faded blue")]),
+            ("shiny gold", [(1, "dark olive"), (2, "vibrant plum")]),
+            ("dark olive", [(3, "faded blue"), (4, "dotted black")]),
+            ("vibrant plum", [(5, "faded blue"), (6, "dotted black")]),
+            ("faded blue", []),
+            ("dotted black", [])
+          ] ::
+          Rules
+
   describe "parseRules" $
     context "given the file example.txt" $
       it "parses the rules" $ do
         input <- readFile "./test/Day07/example.txt"
-        let expected =
-              Right
-                [ Rule "light red" [(1, "bright white"), (2, "muted yellow")],
-                  Rule "dark orange" [(3, "bright white"), (4, "muted yellow")],
-                  Rule "bright white" [(1, "shiny gold")],
-                  Rule "muted yellow" [(2, "shiny gold"), (9, "faded blue")],
-                  Rule "shiny gold" [(1, "dark olive"), (2, "vibrant plum")],
-                  Rule "dark olive" [(3, "faded blue"), (4, "dotted black")],
-                  Rule "vibrant plum" [(5, "faded blue"), (6, "dotted black")],
-                  Rule "faded blue" [],
-                  Rule "dotted black" []
+
+        parseRules input `shouldBe` Right parsedExample
+
+  describe "asTree" $ do
+    context "given the parsedExample" $ do
+      it "builds a Tree" $ do
+        asTree "shiny gold" parsedExample
+          `shouldBe` Map.fromList
+            [ ( "bright white",
+                [ Leaf (1, "shiny gold")
                 ]
-        parseRules input `shouldBe` expected
+              ),
+              ( "dark olive",
+                [ Tree (3, "faded blue") (Just []),
+                  Tree (4, "dotted black") (Just [])
+                ]
+              ),
+              ( "dark orange",
+                [ Tree (3, "bright white") (Just [Leaf (1, "shiny gold")]),
+                  Tree
+                    (4, "muted yellow")
+                    ( Just
+                        [ Leaf (2, "shiny gold"),
+                          Tree (9, "faded blue") (Just [])
+                        ]
+                    )
+                ]
+              ),
+              ("dotted black", []),
+              ("faded blue", []),
+              ( "light red",
+                [ Tree (1, "bright white") (Just [Leaf (1, "shiny gold")]),
+                  Tree
+                    (2, "muted yellow")
+                    ( Just
+                        [ Leaf (2, "shiny gold"),
+                          Tree (9, "faded blue") (Just [])
+                        ]
+                    )
+                ]
+              ),
+              ( "muted yellow",
+                [ Leaf (2, "shiny gold"),
+                  Tree (9, "faded blue") (Just [])
+                ]
+              ),
+              ( "shiny gold",
+                [ Tree
+                    (1, "dark olive")
+                    ( Just
+                        [ Tree (3, "faded blue") (Just []),
+                          Tree (4, "dotted black") (Just [])
+                        ]
+                    ),
+                  Tree
+                    (2, "vibrant plum")
+                    ( Just
+                        [ Tree (5, "faded blue") (Just []),
+                          Tree (6, "dotted black") (Just [])
+                        ]
+                    )
+                ]
+              ),
+              ( "vibrant plum",
+                [ Tree (5, "faded blue") (Just []),
+                  Tree (6, "dotted black") (Just [])
+                ]
+              )
+            ]
+
+  describe "expand" $ do
+    let cases =
+          [ ( Leaf (1, "shiny gold"),
+              [ [(1, "shiny gold")]
+              ]
+            ),
+            ( Tree
+                (4, "muted yellow")
+                ( Just
+                    [ Leaf (2, "shiny gold"),
+                      Tree (9, "faded blue") (Just [])
+                    ]
+                ),
+              [ [(4, "muted yellow"), (2, "shiny gold")],
+                [(4, "muted yellow"), (9, "faded blue")]
+              ]
+            )
+          ]
+        test (input, expected) = it "expands a Tree into a path" $ do
+          expand input `shouldBe` expected
+     in for_ cases test
+  xdescribe "pathsToTarget" $ do
+    context "given the parsedExample" $ do
+      it "finds paths to the target" $ do
+        pathsToTarget "shiny gold" parsedExample
+          `shouldBe` Map.fromList
+            [ ("light red", Just [[(1, "bright white"), (1, "shiny gold")], [(2, "muted yellow"), (2, "shiny gold")]]),
+              ("dark orange", Just [[(3, "bright white"), (1, "shiny gold")], [(4, "muted yellow"), (2, "shiny gold")]]),
+              ("bright white", Just [[(1, "shiny gold")]]),
+              ("muted yellow", Just [[(2, "shiny gold")]]),
+              ("shiny gold", Just []),
+              ("dark olive", Nothing),
+              ("vibrant plum", Nothing),
+              ("faded blue", Nothing),
+              ("dotted black", Nothing)
+            ]
