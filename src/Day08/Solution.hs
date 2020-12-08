d3b9dc2452fd6b94b62129d68556562d94eea6c9
Manu Phatak <manu.phatak@procore.com>
2020-12-07 22:43:56 -0800

    Solve part 1

--- src/Day08/Solution.hs
+++ src/Day08/Solution.hs
@@ -1,7 +1,78 @@
-module Day08.Solution (part1, part2) where
+module Day08.Solution
+  ( part1,
+    part2,
+    Instruction (..),
+    Sign (..),
+    Operation (..),
+    Program (..),
+    parseInstructions,
+    runProgram,
+    initialState,
+  )
+where
+
+import Advent.Utils (readInt)
+import Data.Either (fromRight)
+import qualified Data.IntSet as IntSet
+import Debug.Trace
+import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . programAcc . runProgram initialState . fromRight [] . parseInstructions
 
 part2 :: String -> String
 part2 = head . lines
+
+data Sign = Plus | Minus deriving (Show, Eq)
+
+data Operation = NoOperation | Accumulator | Jump deriving (Show, Eq)
+
+data Instruction = Instruction Operation Sign Int deriving (Show, Eq)
+
+parseInstructions :: String -> Either ParseError [Instruction]
+parseInstructions = parse (instructionParser `sepEndBy1` endOfLine) ""
+
+instructionParser :: Parsec String () Instruction
+instructionParser = Instruction <$> (operationParser <* space) <*> signParser <*> intParser
+
+operationParser :: Parsec String () Operation
+operationParser = readOperation <$> many1 letter
+  where
+    readOperation "nop" = NoOperation
+    readOperation "acc" = Accumulator
+    readOperation "jmp" = Jump
+
+signParser :: Parsec String () Sign
+signParser = readSign <$> choice [char '+', char '-']
+  where
+    readSign '+' = Plus
+    readSign '-' = Minus
+
+intParser :: Parsec String () Int
+intParser = readInt <$> many1 digit
+
+data Program = Program {programAcc :: Int, programPointer :: Int, programVisited :: IntSet.IntSet} deriving (Show)
+
+initialState :: Program
+initialState = Program {programAcc = 0, programPointer = 0, programVisited = IntSet.empty}
+
+runProgram :: Program -> [Instruction] -> Program
+runProgram program instructions
+  | programPointer program `IntSet.member` programVisited program = program
+  | otherwise = runProgram (go (instructions !! programPointer program)) instructions
+  where
+    nextProgram = program {programVisited = programPointer program `IntSet.insert` programVisited program}
+    go :: Instruction -> Program
+    go (Instruction NoOperation _ _) = nextProgram {programPointer = (succ . programPointer) program}
+    go (Instruction Jump Plus n) = nextProgram {programPointer = ((+) n . programPointer) program}
+    go (Instruction Jump Minus n) = nextProgram {programPointer = (subtract n . programPointer) program}
+    go (Instruction Accumulator Plus n) =
+      nextProgram
+        { programPointer = (succ . programPointer) nextProgram,
+          programAcc = ((+) n . programAcc) nextProgram
+        }
+    go (Instruction Accumulator Minus n) =
+      nextProgram
+        { programPointer = (succ . programPointer) nextProgram,
+          programAcc = (subtract n . programAcc) nextProgram
+        }
--- test/Day08/SolutionSpec.hs
+++ test/Day08/SolutionSpec.hs
@@ -1,13 +1,43 @@
 module Day08.SolutionSpec (spec) where
 
-import Day08.Solution (part1, part2)
+import Day08.Solution
+  ( Instruction (..),
+    Operation (..),
+    Program (..),
+    Sign (..),
+    initialState,
+    parseInstructions,
+    part1,
+    part2,
+    runProgram,
+  )
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day08/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "1317"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day08/input.txt"
     part2 input `shouldBe` "hello santa"
+  let parsedExample =
+        [ Instruction NoOperation Plus 0,
+          Instruction Accumulator Plus 1,
+          Instruction Jump Plus 4,
+          Instruction Accumulator Plus 3,
+          Instruction Jump Minus 3,
+          Instruction Accumulator Minus 99,
+          Instruction Accumulator Plus 1,
+          Instruction Jump Minus 4,
+          Instruction Accumulator Plus 6
+        ]
+  describe "parseInstructions" $ do
+    it "parses the example into instructions" $ do
+      input <- readFile "./test/Day08/example.txt"
+      parseInstructions input `shouldBe` Right parsedExample
+
+  describe "runProgram" $ do
+    context "given instructions from example.txt" $ do
+      it "has an accumulator of 5" $ do
+        programAcc (runProgram initialState parsedExample) `shouldBe` 5
