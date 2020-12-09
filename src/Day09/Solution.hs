1834f222506d6228e0ad4854cd571e1db50aa125
Manu Phatak <manu.phatak@procore.com>
2020-12-08 22:06:49 -0800

    Solve part 1

--- src/Day09/Solution.hs
+++ src/Day09/Solution.hs
@@ -1,7 +1,27 @@
-module Day09.Solution (part1, part2) where
+module Day09.Solution (part1, part2, xmasCypher) where
+
+import Advent.Utils (combinations, readInt)
+import Data.Foldable (find)
+import Data.Maybe (fromJust)
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . fromJust . xmasCypher 25
 
 part2 :: String -> String
 part2 = head . lines
+
+xmasCypher :: Int -> String -> Maybe Int
+xmasCypher n = fmap snd . find (uncurry followsPreamble) . rollingChunks n . parseNumbers
+  where
+    followsPreamble :: [Int] -> Int -> Bool
+    followsPreamble preamble target = not $ any ((== target) . sum) (combinations 2 preamble)
+
+parseNumbers :: String -> [Int]
+parseNumbers = map readInt . lines
+
+rollingChunks :: Num b => Int -> [b] -> [([b], b)]
+rollingChunks n xs =
+  [ (take n chunkStart, chunkStart !! max 0 n)
+    | i <- [1 .. (length xs - (n + 1))],
+      let chunkStart = drop i xs
+  ]
--- test/Day09/SolutionSpec.hs
+++ test/Day09/SolutionSpec.hs
@@ -1,13 +1,17 @@
 module Day09.SolutionSpec (spec) where
 
-import Day09.Solution (part1, part2)
+import Day09.Solution (part1, part2, xmasCypher)
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day09/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "167829540"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day09/input.txt"
     part2 input `shouldBe` "hello santa"
+  describe "xmasCypher" $ do
+    it "finds the first number that does not follow the preamble" $ do
+      input <- readFile "./test/Day09/example.txt"
+      xmasCypher 5 input `shouldBe` Just 127
