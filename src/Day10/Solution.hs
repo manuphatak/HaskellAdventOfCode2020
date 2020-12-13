08a3e14d20753a86c23594a53c894f846e7231e1
Manu Phatak <manu.phatak@procore.com>
2020-12-12 20:26:16 -0800

    Almost solve part 2

--- src/Day10/Solution.hs
+++ src/Day10/Solution.hs
@@ -1,16 +1,17 @@
-module Day10.Solution where
+module Day10.Solution (joltageJumps, part1, part2, possibilitiesTree) where
 
 import Advent.Utils (parseInts)
 import Data.List (sort)
+import Data.Tree (Tree, unfoldTree)
 
 part1 :: String -> String
 part1 = show . (\(a, _, c) -> a * c) . joltageJumps
 
 part2 :: String -> String
-part2 = head . lines
+part2 = show . succ . sum . possibilitiesTree
 
 joltageJumps :: String -> (Int, Int, Int)
-joltageJumps = go (0, 0, 1) . (0 :) . sort . parseInts
+joltageJumps = go (0, 0, 0) . sort . withMinMax . parseInts
   where
     go :: (Int, Int, Int) -> [Int] -> (Int, Int, Int)
     go jumps [_] = jumps
@@ -20,3 +21,29 @@ joltageJumps = go (0, 0, 1) . (0 :) . sort . parseInts
           1 -> (succ a, b, c)
           2 -> (a, succ b, c)
           3 -> (a, b, succ c)
+
+possibilitiesTree :: String -> Tree Int
+possibilitiesTree = unfoldTree go . sort . withMinMax . parseInts
+  where
+    go :: [Int] -> (Int, [[Int]])
+    go [] = (0, [])
+    go [_] = (0, [])
+    go (x : xs) =
+      (pred $ length possibilities, possibilities)
+      where
+        possibilities :: [[Int]]
+        possibilities =
+          [ xs'
+            | i <- [0 .. 2],
+              let xs' = next x (drop i xs),
+              not (null xs')
+          ]
+
+    next :: Int -> [Int] -> [Int]
+    next _ [] = []
+    next y ys
+      | head ys - y <= 3 = ys
+      | otherwise = []
+
+withMinMax :: [Int] -> [Int]
+withMinMax = (0 :) . ((:) =<< (3 +) . maximum)
--- test/Day10/SolutionSpec.hs
+++ test/Day10/SolutionSpec.hs
@@ -1,6 +1,6 @@
 module Day10.SolutionSpec (spec) where
 
-import Day10.Solution
+import Day10.Solution (joltageJumps, part1, part2, possibilitiesTree)
 import Test.Hspec
 
 spec :: Spec
@@ -8,6 +8,7 @@ spec = parallel $ do
   it "solves Part 1" $ do
     input <- readFile "./test/Day10/input.txt"
     part1 input `shouldBe` "2376"
+
   xit "solves Part 2" $ do
     input <- readFile "./test/Day10/input.txt"
     part2 input `shouldBe` "hello santa"
@@ -24,3 +25,16 @@ spec = parallel $ do
         input <- readFile "./test/Day10/example-2.txt"
 
         joltageJumps input `shouldBe` expected
+  describe "possibleArrangements" $ do
+    context "given the file example-1.txt" $ do
+      let count = 8
+      it ("has " ++ show count ++ " possible arrangements") $ do
+        input <- readFile "./test/Day10/example-1.txt"
+
+        (succ . sum . possibilitiesTree) input `shouldBe` count
+    context "given the file example-2.txt" $ do
+      let count = 19208
+      it ("has " ++ show count ++ " possible arrangements") $ do
+        input <- readFile "./test/Day10/example-2.txt"
+
+        (succ . sum . possibilitiesTree) input `shouldBe` count
