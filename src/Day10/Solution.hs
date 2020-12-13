1fae0671ff7a30db35d15418ad8711d60c3043f9
Manu Phatak <manu.phatak@procore.com>
2020-12-12 16:11:46 -0800

    Solve part 1

--- src/Day10/Solution.hs
+++ src/Day10/Solution.hs
@@ -1,7 +1,22 @@
-module Day10.Solution (part1, part2) where
+module Day10.Solution where
+
+import Advent.Utils (parseInts)
+import Data.List (sort)
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . (\(a, _, c) -> a * c) . joltageJumps
 
 part2 :: String -> String
 part2 = head . lines
+
+joltageJumps :: String -> (Int, Int, Int)
+joltageJumps = go (0, 0, 1) . (0 :) . sort . parseInts
+  where
+    go :: (Int, Int, Int) -> [Int] -> (Int, Int, Int)
+    go jumps [_] = jumps
+    go (a, b, c) (x : x' : xs) = go nextJumps (x' : xs)
+      where
+        nextJumps = case x' - x of
+          1 -> (succ a, b, c)
+          2 -> (a, succ b, c)
+          3 -> (a, b, succ c)
--- test/Day10/SolutionSpec.hs
+++ test/Day10/SolutionSpec.hs
@@ -1,13 +1,26 @@
 module Day10.SolutionSpec (spec) where
 
-import Day10.Solution (part1, part2)
+import Day10.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day10/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "2376"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day10/input.txt"
     part2 input `shouldBe` "hello santa"
+  describe "joltageJumps" $ do
+    context "given the file example-1.txt" $ do
+      let expected = (7, 0, 5)
+      it ("has joltage jumps of" ++ show expected) $ do
+        input <- readFile "./test/Day10/example-1.txt"
+
+        joltageJumps input `shouldBe` expected
+    context "given the file example-2.txt" $ do
+      let expected = (22, 0, 10)
+      it ("has joltage jumps of" ++ show expected) $ do
+        input <- readFile "./test/Day10/example-2.txt"
+
+        joltageJumps input `shouldBe` expected
