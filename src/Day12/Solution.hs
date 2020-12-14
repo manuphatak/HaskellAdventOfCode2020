eb4eb92a7d9ce51171599ef82683d8fee278c0c8
Manu Phatak <manu.phatak@procore.com>
2020-12-14 00:12:34 -0800

    solve part 1

--- src/Day12/Solution.hs
+++ src/Day12/Solution.hs
@@ -1,10 +1,20 @@
 module Day12.Solution where
 
-import Advent.Utils (readInt)
+import Advent.Utils (fromRightOrShowError, readInt)
+import Debug.Trace
 import Text.Parsec
+  ( ParseError,
+    Parsec,
+    digit,
+    endOfLine,
+    many,
+    oneOf,
+    parse,
+    sepEndBy1,
+  )
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . manhattanDistance . getPosition . run initialState . fromRightOrShowError . parseInstructions
 
 part2 :: String -> String
 part2 = head . lines
@@ -26,3 +36,49 @@ actionParser = read . (: []) <$> oneOf ['N', 'E', 'S', 'W', 'F', 'R', 'L']
 
 valueParser :: Parsec String () Value
 valueParser = readInt <$> many digit
+
+data Direction = North | East | South | West deriving (Enum, Bounded, Eq, Show)
+
+data Point = Point Int Int deriving (Eq, Show)
+
+data State = State {getHeading :: Direction, getPosition :: Point} deriving (Show)
+
+initialState :: State
+initialState = State East (Point 0 0)
+
+run :: State -> [Instruction] -> State
+run = foldl go
+  where
+    go :: State -> Instruction -> State
+    go state (R, 90) = state {getHeading = (next . getHeading) state}
+    go state (R, 180) = state {getHeading = (next . next . getHeading) state}
+    go state (R, 270) = state {getHeading = (next . next . next . getHeading) state}
+    go state (L, 90) = state {getHeading = (prev . getHeading) state}
+    go state (L, 180) = state {getHeading = (prev . prev . getHeading) state}
+    go state (L, 270) = state {getHeading = (prev . prev . prev . getHeading) state}
+    go (State heading point) (F, value) = State heading (nextPoint heading point value)
+    go (State heading point) (N, value) = State heading (nextPoint North point value)
+    go (State heading point) (E, value) = State heading (nextPoint East point value)
+    go (State heading point) (S, value) = State heading (nextPoint South point value)
+    go (State heading point) (W, value) = State heading (nextPoint West point value)
+    go _ _ = error "unknown"
+
+    nextPoint :: Direction -> Point -> Value -> Point
+    nextPoint direction (Point x y) value = traceShowId $ case direction of
+      North -> Point x (y + value)
+      East -> Point (x + value) y
+      South -> Point x (y - value)
+      West -> Point (x - value) y
+
+manhattanDistance :: Point -> Int
+manhattanDistance (Point x y) = abs x + abs y
+
+next :: (Eq p, Bounded p, Enum p) => p -> p
+next e
+  | e == maxBound = minBound
+  | otherwise = succ e
+
+prev :: (Eq p, Bounded p, Enum p) => p -> p
+prev e
+  | e == minBound = maxBound
+  | otherwise = pred e
--- test/Day12/SolutionSpec.hs
+++ test/Day12/SolutionSpec.hs
@@ -5,9 +5,9 @@ import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day12/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "1645"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day12/input.txt"
     part2 input `shouldBe` "hello santa"
@@ -18,3 +18,9 @@ spec = parallel $ do
       it "parses a waiting area" $ do
         input <- readFile "./test/Day12/example.txt"
         parseInstructions input `shouldBe` Right exampleInstructions
+  describe "run" $ do
+    it "runs through the instructions" $ do
+      (getPosition . run initialState) exampleInstructions `shouldBe` Point 17 (-8)
+  describe "manhattanDistance" $ do
+    it "finds the distance of a point" $ do
+      manhattanDistance (Point 17 (-8)) `shouldBe` 25
