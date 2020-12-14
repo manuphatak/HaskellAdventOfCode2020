bb101081e0af2542d7c71fa1b38d8f9910e97fdc
Manu Phatak <manu.phatak@procore.com>
2020-12-13 20:43:28 -0800

    Build parser for part 1

--- src/Day12/Solution.hs
+++ src/Day12/Solution.hs
@@ -1,7 +1,28 @@
-module Day12.Solution (part1, part2) where
+module Day12.Solution where
+
+import Advent.Utils (readInt)
+import Text.Parsec
 
 part1 :: String -> String
 part1 = head . lines
 
 part2 :: String -> String
 part2 = head . lines
+
+type Value = Int
+
+data Action = N | E | S | W | F | R | L deriving (Show, Eq, Read)
+
+type Instruction = (Action, Value)
+
+parseInstructions :: String -> Either ParseError [Instruction]
+parseInstructions = parse (instructionParser `sepEndBy1` endOfLine) ""
+
+instructionParser :: Parsec String () Instruction
+instructionParser = (,) <$> actionParser <*> valueParser
+
+actionParser :: Parsec String () Action
+actionParser = read . (: []) <$> oneOf ['N', 'E', 'S', 'W', 'F', 'R', 'L']
+
+valueParser :: Parsec String () Value
+valueParser = readInt <$> many digit
--- test/Day12/SolutionSpec.hs
+++ test/Day12/SolutionSpec.hs
@@ -1,6 +1,6 @@
 module Day12.SolutionSpec (spec) where
 
-import Day12.Solution (part1, part2)
+import Day12.Solution
 import Test.Hspec
 
 spec :: Spec
@@ -11,3 +11,10 @@ spec = parallel $ do
   xit "solves Part 2" $ do
     input <- readFile "./test/Day12/input.txt"
     part2 input `shouldBe` "hello santa"
+  let exampleInstructions :: [Instruction]
+      exampleInstructions = [(F, 10), (N, 3), (F, 7), (R, 90), (F, 11)]
+  describe "parseInstructions" $ do
+    context "given the input from 'example.txt'" $ do
+      it "parses a waiting area" $ do
+        input <- readFile "./test/Day12/example.txt"
+        parseInstructions input `shouldBe` Right exampleInstructions
