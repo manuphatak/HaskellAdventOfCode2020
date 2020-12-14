b87abd8b74cce7934e0de3e9af48aed521fb1f2e
Manu Phatak <manu.phatak@procore.com>
2020-12-13 18:03:50 -0800

    Solve part 1

--- src/Day11/Solution.hs
+++ src/Day11/Solution.hs
@@ -1,7 +1,69 @@
-module Day11.Solution (part1, part2) where
+module Day11.Solution where
+
+import Advent.Utils (fromRightOrShowError, occurrences)
+import Data.Bifunctor
+import qualified Data.Map.Strict as Map
+import Data.Maybe
+import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . occurrences OccupiedSeat . runSimulation . fromRightOrShowError . parseWaitingArea
 
 part2 :: String -> String
 part2 = head . lines
+
+runSimulation :: WaitingArea -> WaitingArea
+runSimulation waitingArea = if waitingArea == nextWaitingArea then nextWaitingArea else runSimulation nextWaitingArea
+  where
+    nextWaitingArea :: WaitingArea
+    nextWaitingArea = Map.mapWithKey nextSeat waitingArea
+    nextSeat :: Point -> Token -> Token
+    nextSeat point EmptySeat | OccupiedSeat `notElem` adjacentSeats point = OccupiedSeat
+    nextSeat point OccupiedSeat | (>= 4) . occurrences OccupiedSeat $ adjacentSeats point = EmptySeat
+    nextSeat _ token = token
+    adjacentSeats :: Point -> [Token]
+    adjacentSeats (Point x y) =
+      mapMaybe (`Map.lookup` waitingArea) $
+        [ Point (x + i) (y + j)
+          | i <- [-1 .. 1],
+            j <- [-1 .. 1],
+            (i, j) /= (0, 0)
+        ]
+
+adjacentSeats waitingArea (Point x y) =
+  mapMaybe (`Map.lookup` waitingArea) $
+    [ Point (x + i) (y + j)
+      | i <- [-1 .. 1],
+        j <- [-1 .. 1],
+        (i, j) /= (0, 0)
+    ]
+
+type WaitingArea = Map.Map Point Token
+
+data Point = Point Int Int deriving (Show, Eq, Ord)
+
+data Token = Floor | EmptySeat | OccupiedSeat deriving (Show, Eq)
+
+parseWaitingArea :: String -> Either ParseError WaitingArea
+parseWaitingArea = parse waitingAreaParser ""
+
+waitingAreaParser :: Parsec String () WaitingArea
+waitingAreaParser = asWaitingArea . zip [0 ..] <$> (zip [0 ..] <$> rowParser) `sepEndBy1` newline
+  where
+    asWaitingArea :: [(Int, [(Int, Token)])] -> WaitingArea
+    asWaitingArea = Map.fromList . filter rejectFloor . asPoints
+    asPoints :: [(Int, [(Int, Token)])] -> [(Point, Token)]
+    asPoints = concatMap (uncurry (map . first . Point))
+    rejectFloor :: (Point, Token) -> Bool
+    rejectFloor = (Floor /=) . snd
+
+rowParser :: Parsec String () [Token]
+rowParser = many1 tokenParser
+
+tokenParser :: Parsec String () Token
+tokenParser = asToken <$> oneOf "L#."
+  where
+    asToken :: Char -> Token
+    asToken 'L' = EmptySeat
+    asToken '#' = OccupiedSeat
+    asToken _ = Floor
--- test/Day11/SolutionSpec.hs
+++ test/Day11/SolutionSpec.hs
@@ -1,13 +1,26 @@
 module Day11.SolutionSpec (spec) where
 
-import Day11.Solution (part1, part2)
+import Advent.Utils (occurrences)
+import qualified Data.Map.Strict as Map
+import Day11.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day11/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "2243"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day11/input.txt"
     part2 input `shouldBe` "hello santa"
+  let parsedWaitingArea :: WaitingArea
+      parsedWaitingArea = Map.fromList [(Point 0 0, EmptySeat), (Point 0 2, EmptySeat), (Point 0 3, EmptySeat), (Point 0 5, EmptySeat), (Point 0 6, EmptySeat), (Point 0 8, EmptySeat), (Point 0 9, EmptySeat), (Point 1 0, EmptySeat), (Point 1 1, EmptySeat), (Point 1 2, EmptySeat), (Point 1 3, EmptySeat), (Point 1 4, EmptySeat), (Point 1 5, EmptySeat), (Point 1 6, EmptySeat), (Point 1 8, EmptySeat), (Point 1 9, EmptySeat), (Point 2 0, EmptySeat), (Point 2 2, EmptySeat), (Point 2 4, EmptySeat), (Point 2 7, EmptySeat), (Point 3 0, EmptySeat), (Point 3 1, EmptySeat), (Point 3 2, EmptySeat), (Point 3 3, EmptySeat), (Point 3 5, EmptySeat), (Point 3 6, EmptySeat), (Point 3 8, EmptySeat), (Point 3 9, EmptySeat), (Point 4 0, EmptySeat), (Point 4 2, EmptySeat), (Point 4 3, EmptySeat), (Point 4 5, EmptySeat), (Point 4 6, EmptySeat), (Point 4 8, EmptySeat), (Point 4 9, EmptySeat), (Point 5 0, EmptySeat), (Point 5 2, EmptySeat), (Point 5 3, EmptySeat), (Point 5 4, EmptySeat), (Point 5 5, EmptySeat), (Point 5 6, EmptySeat), (Point 5 8, EmptySeat), (Point 5 9, EmptySeat), (Point 6 2, EmptySeat), (Point 6 4, EmptySeat), (Point 7 0, EmptySeat), (Point 7 1, EmptySeat), (Point 7 2, EmptySeat), (Point 7 3, EmptySeat), (Point 7 4, EmptySeat), (Point 7 5, EmptySeat), (Point 7 6, EmptySeat), (Point 7 7, EmptySeat), (Point 7 8, EmptySeat), (Point 7 9, EmptySeat), (Point 8 0, EmptySeat), (Point 8 2, EmptySeat), (Point 8 3, EmptySeat), (Point 8 4, EmptySeat), (Point 8 5, EmptySeat), (Point 8 6, EmptySeat), (Point 8 7, EmptySeat), (Point 8 9, EmptySeat), (Point 9 0, EmptySeat), (Point 9 2, EmptySeat), (Point 9 3, EmptySeat), (Point 9 4, EmptySeat), (Point 9 5, EmptySeat), (Point 9 6, EmptySeat), (Point 9 8, EmptySeat), (Point 9 9, EmptySeat)]
+  describe "parseWaitingArea" $ do
+    context "given the input from 'example.txt'" $ do
+      it "parses a waiting area" $ do
+        input <- readFile "./test/Day11/example.txt"
+        parseWaitingArea input `shouldBe` Right parsedWaitingArea
+  describe "runSimulation" $ do
+    context "given a parseWaitingArea" $ do
+      it "runs the simulation until stabilization" $ do
+        (occurrences OccupiedSeat . runSimulation) parsedWaitingArea `shouldBe` (37 :: Int)
