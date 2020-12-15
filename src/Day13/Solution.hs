ae34bb7bb851eb8c8bd339d30769c242e6d32fb2
Manu Phatak <manu.phatak@procore.com>
2020-12-15 00:19:34 -0800

    Almost solve part 2

--- src/Day13/Solution.hs
+++ src/Day13/Solution.hs
@@ -1,6 +1,7 @@
 module Day13.Solution where
 
 import Advent.Utils
+import Data.Bifunctor
 import Data.Function
 import Data.List
 import Data.Maybe
@@ -10,22 +11,50 @@ part1 :: String -> String
 part1 = show . uncurry (*) . earliestBus . fromRightOrShowError . parseSchedule
 
 part2 :: String -> String
-part2 = head . lines
+part2 = show . busAlignment . snd . fromRightOrShowError . parseSchedule
 
 parseSchedule :: String -> Either ParseError (Int, [Maybe Int])
 parseSchedule = parse scheduleParser ""
   where
     scheduleParser :: Parsec String () (Int, [Maybe Int])
-    scheduleParser = (,) <$> (intParser <* endOfLine) <*> (busIdParser `sepBy1` char ',')
+    scheduleParser = (,) <$> (intParser <* endOfLine) <*> busIdsParser
 
-    intParser :: Parsec String () Int
-    intParser = read <$> many digit
+parseBusIds :: String -> Either ParseError [Maybe Int]
+parseBusIds = parse busIdsParser ""
 
+busIdsParser :: Parsec String () [Maybe Int]
+busIdsParser = busIdParser `sepBy1` char ','
+  where
     busIdParser :: Parsec String () (Maybe Int)
     busIdParser = choice [Nothing <$ try (char 'x'), Just <$> intParser]
+
+intParser :: Parsec String () Int
+intParser = read <$> many digit
 
 earliestBus :: (Int, [Maybe Int]) -> (Int, Int)
 earliestBus (start, busIds) = minimumBy (compare `on` fst) . map nextDeparture . catMaybes $ busIds
   where
     nextDeparture :: Int -> (Int, Int)
     nextDeparture bus = (bus - (start `mod` bus), bus)
+
+busAlignment :: [Maybe Int] -> Int
+busAlignment busIds = go (nextBus - offset)
+  where
+    busses :: [(Int, Int)]
+    busses = sortBy (flip compare `on` snd) . map (second fromJust) . filter (isJust . snd) . zip [0 ..] $ busIds
+
+    (offset, nextBus) = head busses
+
+    maximumIterations = 9999999999
+
+    -- maximumIterations = 999999999999999
+    -- maximumIterations = 100000000000000
+
+    go :: Int -> Int
+    go t
+      | t > maximumIterations = error "timestamp is greater than maximumIterations"
+      | all (match t) busses = t
+      | otherwise = go (t + nextBus)
+
+    match :: Int -> (Int, Int) -> Bool
+    match t (i, bus) = ((t + i) `mod` bus) == 0
--- test/Day13/SolutionSpec.hs
+++ test/Day13/SolutionSpec.hs
@@ -1,5 +1,6 @@
 module Day13.SolutionSpec (spec) where
 
+import Data.Foldable (for_)
 import Day13.Solution
 import Test.Hspec
 
@@ -21,3 +22,19 @@ spec = parallel $ do
   describe "earliestBus" $ do
     it "is" $ do
       earliestBus exampleSchedule `shouldBe` (5, 59)
+
+  describe "busAlignment" $ do
+    let cases =
+          [ ("7,13,x,x,59,x,31,19", 1068781),
+            ("17,x,13,19", 3417),
+            ("67,7,59,61", 754018),
+            ("67,x,7,59,61", 779210),
+            ("67,7,x,59,61", 1261476),
+            ("1789,37,47,1889", 1202161486)
+          ]
+
+    let test (input, expected) = it ("aligns at " ++ show expected ++ " given a schedule of " ++ input) $ do
+          let Right busIds = parseBusIds input
+          busAlignment busIds `shouldBe` expected
+
+    for_ cases test
