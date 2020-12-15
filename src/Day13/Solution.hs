c9d8db186194942e69c25228ab93ce6e515594d5
Manu Phatak <manu.phatak@procore.com>
2020-12-14 22:22:29 -0800

    Solve part 1

--- src/Day13/Solution.hs
+++ src/Day13/Solution.hs
@@ -1,7 +1,31 @@
-module Day13.Solution (part1, part2) where
+module Day13.Solution where
+
+import Advent.Utils
+import Data.Function
+import Data.List
+import Data.Maybe
+import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . uncurry (*) . earliestBus . fromRightOrShowError . parseSchedule
 
 part2 :: String -> String
 part2 = head . lines
+
+parseSchedule :: String -> Either ParseError (Int, [Maybe Int])
+parseSchedule = parse scheduleParser ""
+  where
+    scheduleParser :: Parsec String () (Int, [Maybe Int])
+    scheduleParser = (,) <$> (intParser <* endOfLine) <*> (busIdParser `sepBy1` char ',')
+
+    intParser :: Parsec String () Int
+    intParser = read <$> many digit
+
+    busIdParser :: Parsec String () (Maybe Int)
+    busIdParser = choice [Nothing <$ try (char 'x'), Just <$> intParser]
+
+earliestBus :: (Int, [Maybe Int]) -> (Int, Int)
+earliestBus (start, busIds) = minimumBy (compare `on` fst) . map nextDeparture . catMaybes $ busIds
+  where
+    nextDeparture :: Int -> (Int, Int)
+    nextDeparture bus = (bus - (start `mod` bus), bus)
--- test/Day13/SolutionSpec.hs
+++ test/Day13/SolutionSpec.hs
@@ -1,13 +1,23 @@
 module Day13.SolutionSpec (spec) where
 
-import Day13.Solution (part1, part2)
+import Day13.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day13/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "119"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day13/input.txt"
     part2 input `shouldBe` "hello santa"
+
+  let exampleSchedule = (939, [Just 7, Just 13, Nothing, Nothing, Just 59, Nothing, Just 31, Just 19])
+  describe "parseSchedule" $ do
+    it "is the schedule" $ do
+      input <- readFile "./test/Day13/example.txt"
+      parseSchedule input `shouldBe` Right exampleSchedule
+
+  describe "earliestBus" $ do
+    it "is" $ do
+      earliestBus exampleSchedule `shouldBe` (5, 59)
