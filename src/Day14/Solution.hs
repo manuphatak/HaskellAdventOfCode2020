609a4caf458ba77d699ea627d5c75aed9328f49f
Manu Phatak <manu.phatak@procore.com>
2020-12-15 23:34:55 -0800

    Solve part 1

--- src/Day14/Solution.hs
+++ src/Day14/Solution.hs
@@ -1,7 +1,86 @@
-module Day14.Solution (part1, part2) where
+module Day14.Solution where
+
+import Advent.Utils (fromRightOrShowError, readInt)
+import Data.Bits
+import Data.Foldable
+import qualified Data.IntMap.Strict as IntMap
+import Text.Parsec
+  ( ParseError,
+    Parsec,
+    between,
+    char,
+    choice,
+    count,
+    digit,
+    many1,
+    newline,
+    oneOf,
+    parse,
+    sepEndBy1,
+    spaces,
+    string,
+    try,
+  )
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . sum . stateMemory . runProgram . fromRightOrShowError . parseInstructions
 
 part2 :: String -> String
 part2 = head . lines
+
+data Instruction = Memory Int Int | Mask [Maybe Bool] deriving (Show, Eq)
+
+parseInstructions :: String -> Either ParseError [Instruction]
+parseInstructions = parse (instructionParser `sepEndBy1` newline) ""
+  where
+    instructionParser :: Parsec String () Instruction
+    instructionParser = choice [try maskParser, memoryParser]
+
+    maskParser :: Parsec String () Instruction
+    maskParser = Mask <$> (string "mask = " *> count 36 maskBitParser)
+
+    memoryParser :: Parsec String () Instruction
+    memoryParser = Memory <$> (string "mem" *> between (char '[') (char ']') intParser <* spaces <* char '=' <* spaces) <*> intParser
+
+    maskBitParser :: Parsec String () (Maybe Bool)
+    maskBitParser = fromMask <$> oneOf ['X', '1', '0']
+      where
+        fromMask :: Char -> Maybe Bool
+        fromMask '1' = Just True
+        fromMask '0' = Just False
+        fromMask _ = Nothing
+
+    intParser :: Parsec String () Int
+    intParser = readInt <$> many1 digit
+
+data State = State {stateMask :: [(Int, Bool)], stateMemory :: IntMap.IntMap Int} deriving (Show)
+
+runProgram :: [Instruction] -> State
+runProgram = foldl' go initialState
+  where
+    initialState :: State
+    initialState = State [] IntMap.empty
+
+    go :: State -> Instruction -> State
+    go state (Mask mask) =
+      state
+        { stateMask = catMaybesOnSnd . zip [0 ..] . reverse $ mask
+        }
+    go state (Memory address value) =
+      state
+        { stateMemory = IntMap.insert address (setMemory (stateMask state) value) . stateMemory $ state
+        }
+
+    setMemory :: [(Int, Bool)] -> Int -> Int
+    setMemory ms b = foldr applyMask b ms
+
+    applyMask :: (Int, Bool) -> Int -> Int
+    applyMask (i, True) x = x `setBit` i
+    applyMask (i, False) x = x `clearBit` i
+
+catMaybesOnSnd :: [(Int, Maybe Bool)] -> [(Int, Bool)]
+catMaybesOnSnd = foldr go []
+  where
+    go :: (Int, Maybe Bool) -> [(Int, Bool)] -> [(Int, Bool)]
+    go (_, Nothing) xs = xs
+    go (a, Just b) xs = (a, b) : xs
--- test/Day14/SolutionSpec.hs
+++ test/Day14/SolutionSpec.hs
@@ -1,13 +1,36 @@
 module Day14.SolutionSpec (spec) where
 
-import Day14.Solution (part1, part2)
+import Data.Foldable (for_)
+import qualified Data.IntMap.Strict as IntMap
+import Day14.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  fit "solves Part 1" $ do
     input <- readFile "./test/Day14/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "12408060320841"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day14/input.txt"
     part2 input `shouldBe` "hello santa"
+  let exampleInstructions =
+        [ Mask [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Just True, Nothing, Nothing, Nothing, Nothing, Just False, Nothing],
+          Memory 8 11,
+          Memory 7 101,
+          Memory 8 0
+        ]
+  describe "parseInstructions" $ do
+    it "parses instructions from text" $ do
+      input <- readFile "./test/Day14/example.txt"
+      parseInstructions input `shouldBe` Right exampleInstructions
+
+  describe "runProgram" $ do
+    let cases :: [(Int, Int)]
+        cases = [(7, 101), (8, 64)]
+    let test (address, expected) = it ("has a value of " ++ show expected ++ " at address " ++ show address) $ do
+          (IntMap.lookup address . stateMemory . runProgram) exampleInstructions `shouldBe` Just expected
+
+    for_ cases test
+
+    it "should sum the remaining values" $ do
+      (sum . stateMemory . runProgram) exampleInstructions `shouldBe` 165
