99e04636c104ba1d696995af9e40b1c45c94e99c
Manu Phatak <manu.phatak@procore.com>
2020-12-20 15:36:58 -0800

    Solve part 1

--- src/Day15/Solution.hs
+++ src/Day15/Solution.hs
@@ -1,7 +1,32 @@
-module Day15.Solution (part1, part2) where
+module Day15.Solution where
+
+import Advent.Utils
+import qualified Data.IntMap.Lazy as IntMap
+import Text.Parsec hiding (State)
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . (!! (2020 -1)) . memoryGame . fromRightOrShowError . parseInts
 
 part2 :: String -> String
 part2 = head . lines
+
+parseInts :: String -> Either ParseError [Int]
+parseInts = parse (intsParser <* endOfLine) ""
+  where
+    intsParser :: Parsec String () [Int]
+    intsParser = (readInt <$> many1 digit) `sepBy` char ','
+
+type State = IntMap.IntMap Int
+
+memoryGame :: [Int] -> [Int]
+memoryGame xs = xs ++ go (length xs, last xs, initialState)
+  where
+    go :: (Int, Int, State) -> [Int]
+    go (turn, prev, state) = do
+      let next = maybe 0 (turn -) $ IntMap.lookup prev state
+      let nextState = IntMap.insert prev turn state
+
+      next : go (succ turn, next, nextState)
+
+    initialState :: State
+    initialState = IntMap.fromList $ zip (init xs) [1 ..]
--- test/Day15/SolutionSpec.hs
+++ test/Day15/SolutionSpec.hs
@@ -1,13 +1,37 @@
 module Day15.SolutionSpec (spec) where
 
-import Day15.Solution (part1, part2)
+import Data.Foldable
+import Data.Function
+import Day15.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day15/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "694"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day15/input.txt"
     part2 input `shouldBe` "hello santa"
+  describe "memoryGame" $ do
+    context "when taking the first 10 results" $ do
+      let startingNumbers = [0, 3, 6] :: [Int]
+      let expected = [0, 3, 6, 0, 3, 3, 1, 0, 4, 0] :: [Int]
+      it ("is " ++ show expected ++ " given starting numbers of " ++ show startingNumbers) $ do
+        (memoryGame startingNumbers & take 10) `shouldBe` expected
+
+    context "when looking at the 2020th result" $ do
+      let cases :: [([Int], Int)]
+          cases =
+            [ ([0, 3, 6], 436),
+              ([1, 3, 2], 1),
+              ([2, 1, 3], 10),
+              ([1, 2, 3], 27),
+              ([2, 3, 1], 78),
+              ([3, 2, 1], 438),
+              ([3, 1, 2], 1836)
+            ]
+      let test (input, expected) = it ("is " ++ show expected ++ " for input " ++ show input) $ do
+            (memoryGame input !! (2020 - 1)) `shouldBe` expected
+
+      for_ cases test
