efe7dc499b9080bc84689d3fc36433c24b2ab1f3
Manu Phatak <manu.phatak@procore.com>
2020-12-20 23:41:25 -0800

    Solve part 1

--- src/Day16/Solution.hs
+++ src/Day16/Solution.hs
@@ -1,7 +1,75 @@
-module Day16.Solution (part1, part2) where
+module Day16.Solution where
+
+import Advent.Utils
+import qualified Data.IntMap.Strict as IntMap
+import qualified Data.IntSet as IntSet
+import qualified Data.Map.Strict as Map
+import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . sum . ticketScanningErrors . fromRightOrShowError . parseDocument
 
 part2 :: String -> String
 part2 = head . lines
+
+type Ticket = IntMap.IntMap Int
+
+type DocumentFields = Map.Map String IntSet.IntSet
+
+data Document = Document
+  { dFields :: DocumentFields,
+    dYourTicket :: Ticket,
+    dNearbyTickets :: [Ticket]
+  }
+  deriving (Show, Eq)
+
+parseDocument :: String -> Either ParseError Document
+parseDocument = parse documentParser ""
+
+documentParser :: Parsec String () Document
+documentParser =
+  Document
+    <$> (fieldsParser <* many1 newline)
+    <*> (yourTicketParser <* many1 newline)
+    <*> (nearbyTicketParser <* eof)
+
+fieldsParser :: Parsec String () DocumentFields
+fieldsParser = Map.fromList <$> fieldParser `sepEndBy1` newline
+  where
+    fieldParser :: Parsec String () (String, IntSet.IntSet)
+    fieldParser = (,) <$> (many1 (letter <|> char ' ') <* string ": ") <*> fieldValueParser
+
+    fieldValueParser :: Parsec String () IntSet.IntSet
+    fieldValueParser = IntSet.unions <$> rangeParser `sepBy` string " or "
+
+    rangeParser :: Parsec String () IntSet.IntSet
+    rangeParser =
+      IntSet.fromList <$> do
+        a <- intParser
+        char '-'
+        b <- intParser
+        pure [a .. b]
+
+ticketParser :: Parsec String () Ticket
+ticketParser = toIntMap <$> (intParser `sepBy1` char ',')
+
+yourTicketParser :: Parsec String () Ticket
+yourTicketParser = string "your ticket:" *> newline *> ticketParser
+
+nearbyTicketParser :: Parsec String () [Ticket]
+nearbyTicketParser = string "nearby tickets:" *> newline *> (ticketParser `sepEndBy1` newline)
+
+intParser :: Parsec String () Int
+intParser = readInt <$> many1 digit
+
+toIntMap :: [Int] -> IntMap.IntMap Int
+toIntMap = IntMap.fromList . zip [0 ..]
+
+ticketScanningErrors :: Document -> [Int]
+ticketScanningErrors
+  Document
+    { dFields = fields,
+      dNearbyTickets = nearbyTickets
+    } = filter (`IntSet.notMember` allFields) . concatMap (foldr (:) []) $ nearbyTickets
+    where
+      allFields = foldr1 IntSet.union fields
--- test/Day16/SolutionSpec.hs
+++ test/Day16/SolutionSpec.hs
@@ -1,13 +1,42 @@
 module Day16.SolutionSpec (spec) where
 
-import Day16.Solution (part1, part2)
+import qualified Data.IntSet as IntSet
+import qualified Data.Map.Strict as Map
+import Day16.Solution
 import Test.Hspec
 
 spec :: Spec
-spec = parallel $ do
-  xit "solves Part 1" $ do
-    input <- readFile "./test/Day16/input.txt"
-    part1 input `shouldBe` "hello santa"
-  xit "solves Part 2" $ do
-    input <- readFile "./test/Day16/input.txt"
-    part2 input `shouldBe` "hello santa"
+spec =
+  parallel $ do
+    it "solves Part 1" $ do
+      input <- readFile "./test/Day16/input.txt"
+      part1 input `shouldBe` "21081"
+    xit "solves Part 2" $ do
+      input <- readFile "./test/Day16/input.txt"
+      part2 input `shouldBe` "hello santa"
+    let exampleDocument =
+          Document
+            { dFields =
+                Map.fromList
+                  [ ("class", IntSet.fromList ([1 .. 3] ++ [5 .. 7])),
+                    ("row", IntSet.fromList ([6 .. 11] ++ [33 .. 44])),
+                    ("seat", IntSet.fromList ([13 .. 40] ++ [45 .. 50]))
+                  ],
+              dYourTicket = toIntMap [7, 1, 14],
+              dNearbyTickets =
+                [ toIntMap [7, 3, 47],
+                  toIntMap [40, 4, 50],
+                  toIntMap [55, 2, 20],
+                  toIntMap [38, 6, 12]
+                ]
+            }
+    describe "parseDocument" $ do
+      it "parses rules, your ticket, and nearby tickets" $ do
+        input <- readFile "./test/Day16/example.txt"
+        parseDocument input `shouldBe` Right exampleDocument
+
+    describe "ticketScanningErrors" $ do
+      it "finds invalid ticket values" $ do
+        let output = ticketScanningErrors exampleDocument
+        output `shouldBe` [4, 55, 12]
+        sum output `shouldBe` 71
