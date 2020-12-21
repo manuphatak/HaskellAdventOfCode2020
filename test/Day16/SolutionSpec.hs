be969d4d22f84b8fd5c484b0f6aefb409f840eaf
Manu Phatak <manu.phatak@procore.com>
2020-12-20 17:52:43 -0800

    Setup Day 16

---
+++ test/Day16/SolutionSpec.hs
@@ -0,0 +1,13 @@
+module Day16.SolutionSpec (spec) where
+
+import Day16.Solution (part1, part2)
+import Test.Hspec
+
+spec :: Spec
+spec = parallel $ do
+  xit "solves Part 1" $ do
+    input <- readFile "./test/Day16/input.txt"
+    part1 input `shouldBe` "hello santa"
+  xit "solves Part 2" $ do
+    input <- readFile "./test/Day16/input.txt"
+    part2 input `shouldBe` "hello santa"
