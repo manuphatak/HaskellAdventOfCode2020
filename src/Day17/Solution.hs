698adb2572849dfa80ec9ef86df139d0ea69dfb5
Manu Phatak <manu@phatak.io>
2020-12-23 19:36:05 -0800

    solve part 2

--- src/Day17/Solution.hs
+++ src/Day17/Solution.hs
@@ -10,16 +10,23 @@ part1 :: String -> String
 part1 = show . occurrences Active . executeCycles 6 . fromRightOrShowError . parsePocketDimension
 
 part2 :: String -> String
-part2 = head . lines
-
-type Point = (Int, Int, Int)
+part2 = show . occurrences Active . executeCyclesV2 6 . fromRightOrShowError . parsePocketDimensionV2
 
 data CubeState = Inactive | Active deriving (Show, Eq)
 
+type Point = (Int, Int, Int)
+
+type PointV2 = (Int, Int, Int, Int)
+
 type PocketDimension = Map.Map Point CubeState
+
+type PocketDimensionV2 = Map.Map PointV2 CubeState
 
 parsePocketDimension :: String -> Either ParseError PocketDimension
 parsePocketDimension = parse pocketDimensionParser ""
+
+parsePocketDimensionV2 :: String -> Either ParseError PocketDimensionV2
+parsePocketDimensionV2 = parse pocketDimensionParserV2 ""
 
 pocketDimensionParser :: Parsec String () PocketDimension
 pocketDimensionParser = pocketDimensionBuilder <$> rowParser `sepEndBy1` endOfLine
@@ -28,6 +35,27 @@ pocketDimensionParser = pocketDimensionBuilder <$> rowParser `sepEndBy1` endOfLi
     pocketDimensionBuilder rows =
       Map.fromList
         [ ((x, y, 0), cell)
+          | (y, row) <- zip [0 ..] rows,
+            (x, cell) <- zip [0 ..] row
+        ]
+
+    rowParser :: Parsec String () [CubeState]
+    rowParser = many1 cubeStateParser
+
+    cubeStateParser :: Parsec String () CubeState
+    cubeStateParser = asCubeState <$> oneOf ['#', '.']
+
+    asCubeState :: Char -> CubeState
+    asCubeState '#' = Active
+    asCubeState '.' = Inactive
+
+pocketDimensionParserV2 :: Parsec String () PocketDimensionV2
+pocketDimensionParserV2 = pocketDimensionBuilder <$> rowParser `sepEndBy1` endOfLine
+  where
+    pocketDimensionBuilder :: [[CubeState]] -> PocketDimensionV2
+    pocketDimensionBuilder rows =
+      Map.fromList
+        [ ((x, y, 0, 0), cell)
           | (y, row) <- zip [0 ..] rows,
             (x, cell) <- zip [0 ..] row
         ]
@@ -68,16 +96,55 @@ executeCycles i pocketDimension = executeCycles (pred i) nextPocketDimension
     neighbors :: Point -> Set.Set Point
     neighbors point =
       Set.fromList
-        [ key
+        [ point `offset` ((x, y, z) :: Point)
           | x <- [-1 .. 1],
             y <- [-1 .. 1],
             z <- [-1 .. 1],
-            (x, y, z) /= (0, 0, 0),
-            let key = point `offset` ((x, y, z) :: Point)
+            (x, y, z) /= (0, 0, 0)
         ]
 
     findCube :: Point -> CubeState
     findCube point = Map.findWithDefault Inactive point pocketDimension
 
+executeCyclesV2 :: Int -> PocketDimensionV2 -> PocketDimensionV2
+executeCyclesV2 0 pocketDimension = pocketDimension
+executeCyclesV2 i pocketDimension = executeCyclesV2 (pred i) nextPocketDimension
+  where
+    nextPocketDimension :: PocketDimensionV2
+    nextPocketDimension =
+      Map.fromList
+        [ (point, Active)
+          | point <- allPossiblePoints,
+            Active == cycleCubeState point (findCube point)
+        ]
+
+    allPossiblePoints :: [PointV2]
+    allPossiblePoints = Set.toList . Set.unions . map neighbors . Map.keys . Map.filter (== Active) $ pocketDimension
+
+    cycleCubeState :: PointV2 -> CubeState -> CubeState
+    cycleCubeState point Active
+      | neighbors point & Set.toList & map findCube & occurrences Active & isBetween 2 3 = Active
+      | otherwise = Inactive
+    cycleCubeState point Inactive
+      | neighbors point & Set.toList & map findCube & occurrences Active & (== 3) = Active
+      | otherwise = Inactive
+
+    neighbors :: PointV2 -> Set.Set PointV2
+    neighbors point =
+      Set.fromList
+        [ point `offsetV2` ((x, y, z, w) :: PointV2)
+          | x <- [-1 .. 1],
+            y <- [-1 .. 1],
+            z <- [-1 .. 1],
+            w <- [-1 .. 1],
+            (x, y, z, w) /= (0, 0, 0, 0)
+        ]
+
+    findCube :: PointV2 -> CubeState
+    findCube point = Map.findWithDefault Inactive point pocketDimension
+
 offset :: Point -> Point -> Point
 (a, b, c) `offset` (x, y, z) = (a + x, b + y, c + z)
+
+offsetV2 :: PointV2 -> PointV2 -> PointV2
+(a, b, c, d) `offsetV2` (x, y, z, w) = (a + x, b + y, c + z, d + w)
--- test/Day17/SolutionSpec.hs
+++ test/Day17/SolutionSpec.hs
@@ -10,9 +10,9 @@ spec = parallel $ do
   it "solves Part 1" $ do
     input <- readFile "./test/Day17/input.txt"
     part1 input `shouldBe` "215"
-  xit "solves Part 2" $ do
+  fit "solves Part 2" $ do
     input <- readFile "./test/Day17/input.txt"
-    part2 input `shouldBe` "hello_santa"
+    part2 input `shouldBe` "1728"
   let examplePocketDimension =
         Map.fromList
           [ ((0, 0, 0), Inactive),
@@ -25,15 +25,38 @@ spec = parallel $ do
             ((2, 1, 0), Active),
             ((2, 2, 0), Active)
           ]
+  let examplePocketDimensionV2 =
+        Map.fromList
+          [ ((0, 0, 0, 0), Inactive),
+            ((0, 1, 0, 0), Inactive),
+            ((0, 2, 0, 0), Active),
+            ((1, 0, 0, 0), Active),
+            ((1, 1, 0, 0), Inactive),
+            ((1, 2, 0, 0), Active),
+            ((2, 0, 0, 0), Inactive),
+            ((2, 1, 0, 0), Active),
+            ((2, 2, 0, 0), Active)
+          ]
 
   describe "parsePocketDimension" $ do
     it "parses input" $ do
       input <- readFile "./test/Day17/example.txt"
       parsePocketDimension input `shouldBe` Right examplePocketDimension
+  describe "parsePocketDimensionV2" $ do
+    it "parses input" $ do
+      input <- readFile "./test/Day17/example.txt"
+      parsePocketDimensionV2 input `shouldBe` Right examplePocketDimensionV2
 
   describe "executeCycles" $ do
     let cycles = 6 :: Int
     let expected = 112 :: Int
     context ("when running " ++ show cycles ++ " cycles") $ do
       it ("has " ++ show expected ++ " active cubes") $ do
-        (occurrences Active . executeCycles cycles) examplePocketDimension `shouldBe` 112
+        (occurrences Active . executeCycles cycles) examplePocketDimension `shouldBe` expected
+
+  describe "executeCyclesV2" $ do
+    let cycles = 6 :: Int
+    let expected = 848 :: Int
+    context ("when running " ++ show cycles ++ " cycles") $ do
+      it ("has " ++ show expected ++ " active cubes") $ do
+        (occurrences Active . executeCyclesV2 cycles) examplePocketDimensionV2 `shouldBe` expected
