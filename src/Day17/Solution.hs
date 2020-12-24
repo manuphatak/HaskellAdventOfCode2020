c6def685b4e66a61a71e02485ebf2025dcc59c1d
Manu Phatak <manu@phatak.io>
2020-12-23 19:23:35 -0800

    Solve part 1

--- src/Day17/Solution.hs
+++ src/Day17/Solution.hs
@@ -1,7 +1,83 @@
-module Day17.Solution (part1, part2) where
+module Day17.Solution where
+
+import Advent.Utils
+import Data.Function
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . occurrences Active . executeCycles 6 . fromRightOrShowError . parsePocketDimension
 
 part2 :: String -> String
 part2 = head . lines
+
+type Point = (Int, Int, Int)
+
+data CubeState = Inactive | Active deriving (Show, Eq)
+
+type PocketDimension = Map.Map Point CubeState
+
+parsePocketDimension :: String -> Either ParseError PocketDimension
+parsePocketDimension = parse pocketDimensionParser ""
+
+pocketDimensionParser :: Parsec String () PocketDimension
+pocketDimensionParser = pocketDimensionBuilder <$> rowParser `sepEndBy1` endOfLine
+  where
+    pocketDimensionBuilder :: [[CubeState]] -> PocketDimension
+    pocketDimensionBuilder rows =
+      Map.fromList
+        [ ((x, y, 0), cell)
+          | (y, row) <- zip [0 ..] rows,
+            (x, cell) <- zip [0 ..] row
+        ]
+
+    rowParser :: Parsec String () [CubeState]
+    rowParser = many1 cubeStateParser
+
+    cubeStateParser :: Parsec String () CubeState
+    cubeStateParser = asCubeState <$> oneOf ['#', '.']
+
+    asCubeState :: Char -> CubeState
+    asCubeState '#' = Active
+    asCubeState '.' = Inactive
+
+executeCycles :: Int -> PocketDimension -> PocketDimension
+executeCycles 0 pocketDimension = pocketDimension
+executeCycles i pocketDimension = executeCycles (pred i) nextPocketDimension
+  where
+    nextPocketDimension :: PocketDimension
+    nextPocketDimension =
+      Map.fromList
+        [ (point, Active)
+          | point <- allPossiblePoints,
+            Active == cycleCubeState point (findCube point)
+        ]
+
+    allPossiblePoints :: [Point]
+    allPossiblePoints = Set.toList . Set.unions . map neighbors . Map.keys . Map.filter (== Active) $ pocketDimension
+
+    cycleCubeState :: Point -> CubeState -> CubeState
+    cycleCubeState point Active
+      | neighbors point & Set.toList & map findCube & occurrences Active & isBetween 2 3 = Active
+      | otherwise = Inactive
+    cycleCubeState point Inactive
+      | neighbors point & Set.toList & map findCube & occurrences Active & (== 3) = Active
+      | otherwise = Inactive
+
+    neighbors :: Point -> Set.Set Point
+    neighbors point =
+      Set.fromList
+        [ key
+          | x <- [-1 .. 1],
+            y <- [-1 .. 1],
+            z <- [-1 .. 1],
+            (x, y, z) /= (0, 0, 0),
+            let key = point `offset` ((x, y, z) :: Point)
+        ]
+
+    findCube :: Point -> CubeState
+    findCube point = Map.findWithDefault Inactive point pocketDimension
+
+offset :: Point -> Point -> Point
+(a, b, c) `offset` (x, y, z) = (a + x, b + y, c + z)
--- test/Day17/SolutionSpec.hs
+++ test/Day17/SolutionSpec.hs
@@ -1,13 +1,39 @@
 module Day17.SolutionSpec (spec) where
 
-import Day17.Solution (part1, part2)
+import Advent.Utils (occurrences)
+import qualified Data.Map.Strict as Map
+import Day17.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day17/input.txt"
-    part1 input `shouldBe` "hello_santa"
+    part1 input `shouldBe` "215"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day17/input.txt"
     part2 input `shouldBe` "hello_santa"
+  let examplePocketDimension =
+        Map.fromList
+          [ ((0, 0, 0), Inactive),
+            ((0, 1, 0), Inactive),
+            ((0, 2, 0), Active),
+            ((1, 0, 0), Active),
+            ((1, 1, 0), Inactive),
+            ((1, 2, 0), Active),
+            ((2, 0, 0), Inactive),
+            ((2, 1, 0), Active),
+            ((2, 2, 0), Active)
+          ]
+
+  describe "parsePocketDimension" $ do
+    it "parses input" $ do
+      input <- readFile "./test/Day17/example.txt"
+      parsePocketDimension input `shouldBe` Right examplePocketDimension
+
+  describe "executeCycles" $ do
+    let cycles = 6 :: Int
+    let expected = 112 :: Int
+    context ("when running " ++ show cycles ++ " cycles") $ do
+      it ("has " ++ show expected ++ " active cubes") $ do
+        (occurrences Active . executeCycles cycles) examplePocketDimension `shouldBe` 112
