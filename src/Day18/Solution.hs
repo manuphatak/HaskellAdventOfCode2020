6f5cb5cfa98875c205ddb6bd7415642ada1e0b15
Manu Phatak <manu@phatak.io>
2020-12-24 14:40:50 -0800

    Solve part 1

--- src/Day18/Solution.hs
+++ src/Day18/Solution.hs
@@ -1,7 +1,42 @@
-module Day18.Solution (part1, part2) where
+module Day18.Solution where
+
+import Advent.Parser
+import Advent.Utils
+import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . sum . map (evaluateExpression . fromRightOrShowError . parseExpression) . lines
 
 part2 :: String -> String
 part2 = head . lines
+
+data Operator = Plus | Times deriving (Show, Eq)
+
+data Expression
+  = Binary Expression Operator Expression
+  | Number Int
+  deriving (Show, Eq)
+
+parseExpression :: String -> Either ParseError Expression
+parseExpression = parse expressionParser ""
+
+expressionParser :: Parsec String () Expression
+expressionParser = (parensParser <|> numberExpressionParser) `chainl1` operatorParser
+  where
+    parensParser :: Parsec String () Expression
+    parensParser = between (char '(') (char ')') expressionParser
+
+    numberExpressionParser :: Parsec String () Expression
+    numberExpressionParser = Number <$> intParser
+
+    operatorParser :: Parsec String () (Expression -> Expression -> Expression)
+    operatorParser = readOperator <$> (space *> oneOf ['+', '*'] <* space)
+
+    readOperator :: Char -> Expression -> Expression -> Expression
+    readOperator '+' a b = Binary a Plus b
+    readOperator '*' a b = Binary a Times b
+
+evaluateExpression :: Expression -> Int
+evaluateExpression (Number n) = n
+evaluateExpression (Binary a Plus b) = evaluateExpression a + evaluateExpression b
+evaluateExpression (Binary a Times b) = evaluateExpression a * evaluateExpression b
--- test/Day18/SolutionSpec.hs
+++ test/Day18/SolutionSpec.hs
@@ -1,13 +1,137 @@
 module Day18.SolutionSpec (spec) where
 
-import Day18.Solution (part1, part2)
+import Data.Foldable (for_)
+import Day18.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day18/input.txt"
-    part1 input `shouldBe` "hello_santa"
+    part1 input `shouldBe` "6811433855019"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day18/input.txt"
     part2 input `shouldBe` "hello_santa"
+
+  describe "parseExpression" $ do
+    let cases =
+          [ ("1 + 2", 3, Binary (Number 1) Plus (Number 2)),
+            ( "1 + 2 * 3 + 4 * 5 + 6",
+              71,
+              Binary
+                ( Binary
+                    ( Binary
+                        ( Binary
+                            (Binary (Number 1) Plus (Number 2))
+                            Times
+                            (Number 3)
+                        )
+                        Plus
+                        (Number 4)
+                    )
+                    Times
+                    (Number 5)
+                )
+                Plus
+                (Number 6)
+            ),
+            ( "2 * 3 + (4 * 5)",
+              26,
+              Binary
+                (Binary (Number 2) Times (Number 3))
+                Plus
+                (Binary (Number 4) Times (Number 5))
+            ),
+            ( "5 + (8 * 3 + 9 + 3 * 4 * 3)",
+              437,
+              Binary
+                (Number 5)
+                Plus
+                ( Binary
+                    ( Binary
+                        ( Binary
+                            ( Binary
+                                (Binary (Number 8) Times (Number 3))
+                                Plus
+                                (Number 9)
+                            )
+                            Plus
+                            (Number 3)
+                        )
+                        Times
+                        (Number 4)
+                    )
+                    Times
+                    (Number 3)
+                )
+            ),
+            ( "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+              12240,
+              Binary
+                ( Binary (Number 5) Times (Number 9)
+                )
+                Times
+                ( Binary
+                    ( Binary
+                        ( Binary
+                            ( Binary
+                                (Binary (Number 7) Times (Number 3))
+                                Times
+                                (Number 3)
+                            )
+                            Plus
+                            (Number 9)
+                        )
+                        Times
+                        (Number 3)
+                    )
+                    Plus
+                    ( Binary
+                        (Binary (Number 8) Plus (Number 6))
+                        Times
+                        (Number 4)
+                    )
+                )
+            ),
+            ( "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",
+              13632,
+              Binary
+                ( Binary
+                    ( Binary
+                        ( Binary
+                            ( Binary
+                                ( Binary
+                                    (Binary (Number 2) Plus (Number 4))
+                                    Times
+                                    (Number 9)
+                                )
+                                Times
+                                ( Binary
+                                    ( Binary
+                                        (Binary (Number 6) Plus (Number 9))
+                                        Times
+                                        (Number 8)
+                                    )
+                                    Plus
+                                    (Number 6)
+                                )
+                            )
+                            Plus
+                            (Number 6)
+                        )
+                        Plus
+                        (Number 2)
+                    )
+                    Plus
+                    (Number 4)
+                )
+                Times
+                (Number 2)
+            )
+          ]
+    let test (input, evaluated, parsed) = context ("given an expression of " ++ show input) $ do
+          it "can be parsed" $ do
+            parseExpression input `shouldBe` Right parsed
+          it ("is evaluates to " ++ show evaluated) $ do
+            evaluateExpression parsed `shouldBe` evaluated
+    for_ cases test
