ab5879268ef5595cd6b983f9a4f9c20200b2fc37
Manu Phatak <manu@phatak.io>
2020-12-24 18:42:04 -0800

    fix shadow variables warning

--- src/Day18/Internal.hs
+++ src/Day18/Internal.hs
@@ -11,10 +11,9 @@ draw :: Expression -> [String]
 draw n@(Number _) = [show n]
 draw (BinaryOp (a, b, c)) =
   "BinaryOp" :
-  prefix '(' (draw a)
+  shift "  ( " "    " (draw a)
     ++ ["  , " ++ show b]
-    ++ prefix ',' (draw c)
+    ++ shift "  , " "    " (draw c)
     ++ ["  )"]
   where
-    prefix c = shift [' ', ' ', c, ' '] "    "
     shift first other = zipWith (++) (first : repeat other)
