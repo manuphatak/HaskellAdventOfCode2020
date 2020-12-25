76ff0bb95c4d81db6cb10b8b08856c741ca204f4
Manu Phatak <manu@phatak.io>
2020-12-24 17:25:29 -0800

    Solve part 2

--- src/Day18/Solution.hs
+++ src/Day18/Solution.hs
@@ -2,41 +2,80 @@ module Day18.Solution where
 
 import Advent.Parser
 import Advent.Utils
+import Data.Functor.Identity
 import Text.Parsec
+import Text.Parsec.Expr
 
 part1 :: String -> String
-part1 = show . sum . map (evaluateExpression . fromRightOrShowError . parseExpression) . lines
+part1 = show . sum . map (evaluateExpression . fromRightOrShowError . parseExpression basicTable) . lines
 
 part2 :: String -> String
-part2 = head . lines
+part2 = show . sum . map (evaluateExpression . fromRightOrShowError . parseExpression advancedTable) . lines
 
-data Operator = Plus | Times deriving (Show, Eq)
+data Op = Plus | Times deriving (Show, Eq)
 
 data Expression
-  = BinaryOp (Expression, Operator, Expression)
+  = BinaryOp (Expression, Op, Expression)
   | Number Int
   deriving (Show, Eq)
 
-parseExpression :: String -> Either ParseError Expression
-parseExpression = parse expressionParser ""
-
-expressionParser :: Parsec String () Expression
-expressionParser = (parensParser <|> numberExpressionParser) `chainl1` operatorParser
-  where
-    parensParser :: Parsec String () Expression
-    parensParser = between (char '(') (char ')') expressionParser
-
-    numberExpressionParser :: Parsec String () Expression
-    numberExpressionParser = Number <$> intParser
-
-    operatorParser :: Parsec String () (Expression -> Expression -> Expression)
-    operatorParser = readOperator <$> (space *> oneOf ['+', '*'] <* space)
-
-    readOperator :: Char -> Expression -> Expression -> Expression
-    readOperator '+' a b = BinaryOp (a, Plus, b)
-    readOperator '*' a b = BinaryOp (a, Times, b)
+type ExpressionOpTable = OperatorTable String () Identity Expression
 
 evaluateExpression :: Expression -> Int
 evaluateExpression (Number n) = n
 evaluateExpression (BinaryOp (a, Plus, b)) = evaluateExpression a + evaluateExpression b
 evaluateExpression (BinaryOp (a, Times, b)) = evaluateExpression a * evaluateExpression b
+
+parseExpression :: ExpressionOpTable -> String -> Either ParseError Expression
+parseExpression table = parse (expressionParser table) ""
+
+expressionParser :: ExpressionOpTable -> Parsec String () Expression
+expressionParser table = buildExpressionParser table termParser
+  where
+    termParser = parensParser <|> numberExpressionParser
+
+    parensParser :: Parsec String () Expression
+    parensParser = between (char '(') (char ')') (expressionParser table)
+
+    numberExpressionParser :: Parsec String () Expression
+    numberExpressionParser = Number <$> intParser
+
+basicTable :: ExpressionOpTable
+basicTable = [[Infix operatorParser AssocLeft]]
+  where
+    operatorParser :: Parsec String () (Expression -> Expression -> Expression)
+    operatorParser = readOperator <$> (space *> oneOf ['+', '*'] <* space)
+
+advancedTable :: ExpressionOpTable
+advancedTable =
+  [ [Infix (try plusParser) AssocLeft],
+    [Infix (try timesParser) AssocLeft]
+  ]
+  where
+    timesParser :: Parsec String () (Expression -> Expression -> Expression)
+    timesParser = readOperator <$> (space *> char '*' <* space)
+    plusParser :: Parsec String () (Expression -> Expression -> Expression)
+    plusParser = readOperator <$> (space *> char '+' <* space)
+
+readOperator :: Char -> Expression -> Expression -> Expression
+readOperator '+' a b = BinaryOp (a, Plus, b)
+readOperator '*' a b = BinaryOp (a, Times, b)
+
+-- drawExpression :: Expression -> String
+-- drawExpression (BinaryOp (a, b, c)) = unlines ["  ( " ++ show a, "    " ++ show b, "    " ++ show c, "  )"]
+-- drawExpression expression = show expression
+
+-- temp =
+--   BinaryOp
+--     ( Number 2,
+--       Times,
+--       BinaryOp
+--         ( Number 3,
+--           Plus,
+--           BinaryOp
+--             ( Number 4,
+--               Times,
+--               Number 5
+--             )
+--         )
+--     )
--- test/Day18/SolutionSpec.hs
+++ test/Day18/SolutionSpec.hs
@@ -1,144 +1,76 @@
+{-# LANGUAGE TemplateHaskellQuotes #-}
+
 module Day18.SolutionSpec (spec) where
 
 import Data.Foldable (for_)
 import Day18.Solution
 import Test.Hspec
+import Test.Hspec.Golden (defaultGolden)
 
 spec :: Spec
 spec = parallel $ do
   it "solves Part 1" $ do
     input <- readFile "./test/Day18/input.txt"
     part1 input `shouldBe` "6811433855019"
-  xit "solves Part 2" $ do
+  it "solves Part 2" $ do
     input <- readFile "./test/Day18/input.txt"
-    part2 input `shouldBe` "hello_santa"
+    part2 input `shouldBe` "129770152447927"
 
   describe "parseExpression" $ do
-    let cases =
-          [ ( "1 + 2",
-              3,
-              BinaryOp (Number 1, Plus, Number 2)
-            ),
-            ( "1 + 2 * 3 + 4 * 5 + 6",
-              71,
-              BinaryOp
-                ( BinaryOp
-                    ( BinaryOp
-                        ( BinaryOp
-                            ( BinaryOp (Number 1, Plus, Number 2),
-                              Times,
-                              Number 3
-                            ),
-                          Plus,
-                          Number 4
-                        ),
-                      Times,
-                      Number 5
-                    ),
-                  Plus,
-                  Number 6
-                )
-            ),
-            ( "2 * 3 + (4 * 5)",
-              26,
-              BinaryOp
-                ( BinaryOp (Number 2, Times, Number 3),
-                  Plus,
-                  BinaryOp (Number 4, Times, Number 5)
-                )
-            ),
-            ( "5 + (8 * 3 + 9 + 3 * 4 * 3)",
-              437,
-              BinaryOp
-                ( Number 5,
-                  Plus,
-                  BinaryOp
-                    ( BinaryOp
-                        ( BinaryOp
-                            ( BinaryOp
-                                ( BinaryOp (Number 8, Times, Number 3),
-                                  Plus,
-                                  Number 9
-                                ),
-                              Plus,
-                              Number 3
-                            ),
-                          Times,
-                          Number 4
-                        ),
-                      Times,
-                      Number 3
-                    )
-                )
-            ),
-            ( "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
-              12240,
-              BinaryOp
-                ( BinaryOp (Number 5, Times, Number 9),
-                  Times,
-                  BinaryOp
-                    ( BinaryOp
-                        ( BinaryOp
-                            ( BinaryOp
-                                ( BinaryOp (Number 7, Times, Number 3),
-                                  Times,
-                                  Number 3
-                                ),
-                              Plus,
-                              Number 9
-                            ),
-                          Times,
-                          Number 3
-                        ),
-                      Plus,
-                      BinaryOp
-                        ( BinaryOp (Number 8, Plus, Number 6),
-                          Times,
-                          Number 4
-                        )
-                    )
-                )
-            ),
-            ( "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",
-              13632,
-              BinaryOp
-                ( BinaryOp
-                    ( BinaryOp
-                        ( BinaryOp
-                            ( BinaryOp
-                                ( BinaryOp
-                                    ( BinaryOp (Number 2, Plus, Number 4),
-                                      Times,
-                                      Number 9
-                                    ),
-                                  Times,
-                                  BinaryOp
-                                    ( BinaryOp
-                                        ( BinaryOp (Number 6, Plus, Number 9),
-                                          Times,
-                                          Number 8
-                                        ),
-                                      Plus,
-                                      Number 6
-                                    )
-                                ),
-                              Plus,
-                              Number 6
-                            ),
-                          Plus,
-                          Number 2
-                        ),
-                      Plus,
-                      Number 4
-                    ),
-                  Times,
-                  Number 2
-                )
-            )
-          ]
-    let test (input, evaluated, parsed) = context ("given an expression of " ++ show input) $ do
+    let test prefix table (i, (input, expected)) = context ("given an expression of " ++ show input) $ do
           it "can be parsed" $ do
-            parseExpression input `shouldBe` Right parsed
-          it ("is evaluates to " ++ show evaluated) $ do
-            evaluateExpression parsed `shouldBe` evaluated
-    for_ cases test
+            let filename = show 'parseExpression ++ "_" ++ prefix ++ "_" ++ show i
+            let Right output = parseExpression table input
+            defaultGolden filename (show output)
+
+          it ("evaluates to " ++ show expected) $ do
+            (fmap evaluateExpression . parseExpression table) input `shouldBe` Right expected
+
+    context "given the basic math table " $ do
+      let cases =
+            [ ( "1 + 2",
+                3 :: Int
+              ),
+              ( "1 + 2 * 3 + 4 * 5 + 6",
+                71
+              ),
+              ( "1 + (2 * 3) + (4 * (5 + 6))",
+                51
+              ),
+              ( "2 * 3 + (4 * 5)",
+                26
+              ),
+              ( "5 + (8 * 3 + 9 + 3 * 4 * 3)",
+                437
+              ),
+              ( "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+                12240
+              ),
+              ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)
+            ]
+
+      for_ (zip [0 :: Int ..] cases) (test "basic" basicTable)
+    context "given the advanced math table " $ do
+      let cases =
+            [ ( "1 + 2",
+                3 :: Int
+              ),
+              ( "1 + 2 * 3 + 4 * 5 + 6",
+                231
+              ),
+              ( "1 + (2 * 3) + (4 * (5 + 6))",
+                51
+              ),
+              ( "2 * 3 + (4 * 5)",
+                46
+              ),
+              ( "5 + (8 * 3 + 9 + 3 * 4 * 3)",
+                1445
+              ),
+              ( "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+                669060
+              ),
+              ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)
+            ]
+
+      for_ (zip [0 :: Int ..] cases) (test "advanced" advancedTable)
