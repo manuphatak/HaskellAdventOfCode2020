80ce36101141152f27b6eeb400870190a93ebec8
Manu Phatak <manu@phatak.io>
2020-12-25 15:07:53 -0800

    Solve part 1

--- src/Day19/Solution.hs
+++ src/Day19/Solution.hs
@@ -1,7 +1,95 @@
-module Day19.Solution (part1, part2) where
+module Day19.Solution where
+
+import Advent.Parser
+import Advent.Utils
+import Data.Either
+import qualified Data.IntMap.Lazy as IntMap
+import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . length . validMessages . fromRightOrShowError . parseDocument
 
 part2 :: String -> String
 part2 = head . lines
+
+data Rule = Ref [[Int]] | Val Char deriving (Show, Eq)
+
+type Rules = IntMap.IntMap Rule
+
+data Document = Document {dRules :: Rules, dMessages :: [String]} deriving (Show, Eq)
+
+parseDocument :: String -> Either ParseError Document
+parseDocument = parse documentParser ""
+  where
+    documentParser :: Parsec String () Document
+    documentParser = Document <$> rulesParser <*> (spaces *> messagesParser <* eof)
+
+    messagesParser :: Parsec String () [String]
+    messagesParser = many1 letter `sepEndBy` newline
+
+    rulesParser :: Parsec String () Rules
+    rulesParser = IntMap.fromList <$> (rulePairParser `endBy1` newline)
+
+    rulePairParser :: Parsec String () (Int, Rule)
+    rulePairParser = (,) <$> (intParser <* char ':' <* char ' ') <*> ruleParser
+
+    ruleParser :: Parsec String () Rule
+    ruleParser = valParser <|> refParser
+
+    refParser :: Parsec String () Rule
+    refParser = Ref <$> (subRuleParser `sepBy1` (char '|' <* char ' '))
+
+    subRuleParser :: Parsec String () [Int]
+    subRuleParser = intParser `sepEndBy1` char ' '
+
+    valParser :: Parsec String () Rule
+    valParser = Val <$> betweenDblQuotes letter
+
+    betweenDblQuotes :: Parsec String () a -> Parsec String () a
+    betweenDblQuotes = between (char '"') (char '"')
+
+buildDynamicParser :: Rules -> String -> Either ParseError String
+buildDynamicParser rules = parse dynamicParser ""
+  where
+    dynamicParser :: Parsec String () String
+    dynamicParser = go (rules IntMap.! 0) <* eof
+      where
+        go :: Rule -> Parsec String () String
+        go (Ref options) = choice . map (try . mconcat . map (go . (rules IntMap.!))) $ options
+        go (Val c) = string [c]
+
+-- parseTemp :: String -> Either ParseError String
+-- parseTemp = parse tempParser ""
+--   where
+--     tempParser :: Parsec String () String
+--     tempParser =
+--       string "a"
+--         <> choice
+--           [ try
+--               ( choice
+--                   [ try (string "a" <> string "a"),
+--                     try (string "b" <> string "b")
+--                   ]
+--                   <> choice
+--                     [ try (string "a" <> string "b"),
+--                       try (string "b" <> string "a")
+--                     ]
+--               ),
+--             try
+--               ( choice
+--                   [ try (string "a" <> string "b"),
+--                     try (string "b" <> string "a")
+--                   ]
+--                   <> choice
+--                     [ try (string "a" <> string "a"),
+--                       try (string "b" <> string "b")
+--                     ]
+--               )
+--           ]
+--         <> string "b"
+--         <* eof
+
+validMessages :: Document -> [String]
+validMessages (Document rules messages) = filter (isRight . parseDynamic) messages
+  where
+    parseDynamic = buildDynamicParser rules
--- test/Day19/SolutionSpec.hs
+++ test/Day19/SolutionSpec.hs
@@ -1,13 +1,66 @@
+{-# LANGUAGE TemplateHaskellQuotes #-}
+
 module Day19.SolutionSpec (spec) where
 
-import Day19.Solution (part1, part2)
+import Data.Either
+import Data.Foldable (for_)
+import qualified Data.IntMap.Lazy as IntMap
+import Day19.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day19/input.txt"
-    part1 input `shouldBe` "hello_santa"
+    part1 input `shouldBe` "216"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day19/input.txt"
     part2 input `shouldBe` "hello_santa"
+
+  let exampleRules1 =
+        IntMap.fromList
+          [ (0, Ref [[1, 2]]),
+            (1, Val 'a'),
+            (2, Ref [[1, 3], [3, 1]]),
+            (3, Val 'b')
+          ]
+  let exampleRules2 =
+        IntMap.fromList
+          [ (0, Ref [[4, 1, 5]]),
+            (1, Ref [[2, 3], [3, 2]]),
+            (2, Ref [[4, 4], [5, 5]]),
+            (3, Ref [[4, 5], [5, 4]]),
+            (4, Val 'a'),
+            (5, Val 'b')
+          ]
+  let exampleDocument1 = Document exampleRules1 []
+  let exampleDocument2 = Document exampleRules2 ["ababbb", "bababa", "abbbab", "aaabbb", "aaaabbb"]
+  describe "parseDocument" $ do
+    context "given example-1.txt" $ do
+      it "parses rules" $ do
+        input <- readFile "./test/Day19/example-1.txt"
+        parseDocument input `shouldBe` Right exampleDocument1
+    context "given example-2.txt" $ do
+      it "parses rules" $ do
+        input <- readFile "./test/Day19/example-2.txt"
+
+        parseDocument input `shouldBe` Right exampleDocument2
+
+  describe "dynamicParser" $ do
+    context "given example-2.txt" $ do
+      let parseDynamic = buildDynamicParser exampleRules2
+      let cases =
+            [ ("ababbb", isRight),
+              ("bababa", isLeft),
+              ("abbbab", isRight),
+              ("aaabbb", isLeft),
+              ("aaaabbb", isLeft)
+            ]
+      let test (input, matcher) = it ("satisfies matcher for input " ++ show input) $ do
+            parseDynamic input `shouldSatisfy` matcher
+      for_ cases test
+
+  describe "validMessages" $ do
+    context "given example-2.txt" $ do
+      it "selects valid messages" $ do
+        validMessages exampleDocument2 `shouldBe` ["ababbb", "abbbab"]
