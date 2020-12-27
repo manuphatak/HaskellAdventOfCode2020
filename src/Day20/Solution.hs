70ccad8247050912685ec2534c210e1b6a71d7be
Manu Phatak <manu@phatak.io>
2020-12-27 15:15:10 -0800

    Solve part 2

--- src/Day20/Solution.hs
+++ src/Day20/Solution.hs
@@ -2,23 +2,27 @@ module Day20.Solution where
 
 import Advent.Parser
 import Advent.Utils
+import Control.Arrow
 import Data.Foldable (find)
 import Data.Function
 import Data.Functor
 import Data.List (transpose)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
-import Data.Sequence hiding (reverse, take)
+import Data.Sequence (Seq (Empty, (:<|)), fromList, (|>))
 import qualified Data.Set as Set
+import Day09.Utils
 import Text.Parsec hiding (Empty)
 
 part1 :: String -> String
 part1 = show . product . cornerIds . buildGrid . fromRightOrShowError . parseTiles
 
 part2 :: String -> String
-part2 = head . lines
+part2 = show . waterRoughness . ((,) =<< maxSeaMonsterCount) . stitchedImage . buildGrid . fromRightOrShowError . parseTiles
 
-data Tile = Tile {tId :: Int, tContent :: [[Char]]} deriving (Show, Eq)
+type Image = [String]
+
+data Tile = Tile {tId :: Int, tContent :: Image} deriving (Show, Eq)
 
 parseTiles :: String -> Either ParseError (Seq Tile)
 parseTiles = parse (tilesParser <* eof) ""
@@ -27,10 +31,10 @@ parseTiles = parse (tilesParser <* eof) ""
     tilesParser = fromList <$> tileParser `sepEndBy1` newline
 
     tileParser :: Parsec String () Tile
-    tileParser = Tile <$> (string "Tile " *> intParser <* char ':' <* newline) <*> tileContentParser
+    tileParser = Tile <$> (string "Tile " *> intParser <* char ':' <* newline) <*> imageParser
 
-    tileContentParser :: Parsec String () [[Char]]
-    tileContentParser = many1 (oneOf ['.', '#']) `sepEndBy1` newline
+    imageParser :: Parsec String () Image
+    imageParser = many1 (oneOf ['.', '#']) `sepEndBy1` newline
 
 tUp :: Tile -> String
 tUp = head . tContent
@@ -76,15 +80,18 @@ buildGrid (x :<| xs) = go (xs, Map.fromList [((0, 0), x)])
 boundingBox :: Grid -> [Point]
 boundingBox grid = [(minX, minY), (minX, maxY), (maxX, minY), (maxX, maxY)]
   where
+    (minX, maxX, minY, maxY) = bounds grid
+
+bounds :: Grid -> (Int, Int, Int, Int)
+bounds grid =
+  ( gridKeys & map fst & minimum,
+    gridKeys & map fst & maximum,
+    gridKeys & map snd & minimum,
+    gridKeys & map snd & maximum
+  )
+  where
+    gridKeys :: [Point]
     gridKeys = Map.keys grid
-    minX :: Int
-    minX = gridKeys & map fst & minimum
-    maxX :: Int
-    maxX = gridKeys & map fst & maximum
-    minY :: Int
-    minY = gridKeys & map snd & minimum
-    maxY :: Int
-    maxY = gridKeys & map snd & maximum
 
 cornerIds :: Grid -> [Int]
 cornerIds grid = grid & boundingBox & map (`Map.lookup` grid) & catMaybes & map tId
@@ -113,17 +120,84 @@ tileFit grid point tile = find (tileFitsPoint grid point) tileOrientations
 tileFitsPoint :: Grid -> Point -> Tile -> Bool
 tileFitsPoint grid point tile =
   and
-    [ point & add (1, 0) & flip Map.lookup grid <&> tDown & isMatchOn tUp tile,
-      point & add (-1, 0) & flip Map.lookup grid <&> tUp & isMatchOn tDown tile,
-      point & add (0, 1) & flip Map.lookup grid <&> tLeft & isMatchOn tRight tile,
-      point & add (0, -1) & flip Map.lookup grid <&> tRight & isMatchOn tLeft tile
+    [ point & add (0, -1) & flip Map.lookup grid <&> tDown & isMatchOn tUp tile,
+      point & add (0, 1) & flip Map.lookup grid <&> tUp & isMatchOn tDown tile,
+      point & add (1, 0) & flip Map.lookup grid <&> tLeft & isMatchOn tRight tile,
+      point & add (-1, 0) & flip Map.lookup grid <&> tRight & isMatchOn tLeft tile
     ]
   where
     isMatchOn :: Eq a => (t -> a) -> t -> Maybe a -> Bool
     isMatchOn _ _ Nothing = True
     isMatchOn fn t (Just s) = fn t == s
 
-orientations :: [[a]] -> [[[a]]]
+orientations :: Image -> [Image]
 orientations xs = (take 4 . iterate (transpose . reverse) $ xs) ++ (take 4 . iterate (transpose . reverse) $ mirror)
   where
     mirror = map reverse xs
+
+stitchedImage :: Grid -> Image
+stitchedImage grid = combineImages . map (\y -> map (\x -> grid Map.! (x, y) & tContent & stripEdges) [minX .. maxX]) $ [minY .. maxY]
+  where
+    (minX, maxX, minY, maxY) = bounds grid
+
+stripEdges :: Image -> Image
+stripEdges = map (init . tail) . init . tail
+
+combineImages :: Semigroup c => [[[c]]] -> [c]
+combineImages = concatMap (foldr1 (zipWith (<>)))
+
+maxSeaMonsterCount :: Image -> Int
+maxSeaMonsterCount = maximum . map countSeaMonsters . orientations
+
+waterRoughness :: (Int, Image) -> Int
+waterRoughness (c, img) = img & map (occurrences '#') & sum & subtract (seaMonsterCharCount * c)
+
+seaMonsterCharCount :: Int
+seaMonsterCharCount = seaMonsterMatcher & Map.size
+
+seaMonster :: Image
+seaMonster =
+  [ "                  # ",
+    "#    ##    ##    ###",
+    " #  #  #  #  #  #   "
+  ]
+
+imageDimensions :: Image -> (Int, Int)
+imageDimensions img = ((length . head) img, length img)
+
+type ImageMap = Map.Map Point Char
+
+toImageMap :: Image -> ImageMap
+toImageMap =
+  Map.fromList
+    . concatMap
+      ( \(y, rows) ->
+          rows
+            & zip [0 ..]
+            & map
+              ( \(x, cell) ->
+                  ((x, y), cell)
+              )
+      )
+    . zip [0 ..]
+
+seaMonsterMatcher :: ImageMap
+seaMonsterMatcher = seaMonster & toImageMap & Map.filter (/= ' ')
+
+countSeaMonsters :: Image -> Int
+countSeaMonsters img = filter isSeaMonster startPoints & length
+  where
+    startPoints :: [Point]
+    startPoints = [(x, y) | x <- [0 .. (imgW - monsterW)], y <- [0 .. (imgH - monsterH)]]
+
+    isSeaMonster :: Point -> Bool
+    isSeaMonster point = Map.mapWithKey (isMatch point) seaMonsterMatcher & and
+
+    isMatch :: Point -> Point -> Char -> Bool
+    isMatch l r m = Map.lookup (l `add` r) imageMap == Just m
+
+    imageMap :: ImageMap
+    imageMap = toImageMap img
+
+    (imgW, imgH) = imageDimensions img
+    (monsterW, monsterH) = imageDimensions seaMonster
