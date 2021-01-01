28238aa0011096899eea9ffaf2f4177f018c5edd
Manu Phatak <manu@phatak.io>
2021-01-01 15:51:03 -0800

    build a parser

--- src/Day21/Solution.hs
+++ src/Day21/Solution.hs
@@ -1,7 +1,28 @@
-module Day21.Solution (part1, part2) where
+module Day21.Solution where
+
+import qualified Data.Set as Set
+import Text.Parsec
 
 part1 :: String -> String
 part1 = head . lines
 
 part2 :: String -> String
 part2 = head . lines
+
+type Ingredients = Set.Set String
+
+type Allergens = Set.Set String
+
+data Food = Food Ingredients Allergens deriving (Show, Eq)
+
+parseFoods :: String -> Either ParseError [Food]
+parseFoods = parse (foodParser `sepEndBy1` newline) ""
+  where
+    foodParser :: Parsec String () Food
+    foodParser = Food <$> ingredientsParser <*> allergensParser
+
+    ingredientsParser :: Parsec String () Ingredients
+    ingredientsParser = Set.fromList <$> many1 letter `sepEndBy1` space
+
+    allergensParser :: Parsec String () Allergens
+    allergensParser = Set.fromList <$> between (string "(contains ") (char ')') (many1 letter `sepBy` string ", ")
--- test/Day21/SolutionSpec.hs
+++ test/Day21/SolutionSpec.hs
@@ -1,13 +1,25 @@
 module Day21.SolutionSpec (spec) where
 
-import Day21.Solution (part1, part2)
+import qualified Data.Set as Set
+import Day21.Solution
 import Test.Hspec
 
 spec :: Spec
-spec = parallel $ do
+spec = focus . parallel $ do
   xit "solves Part 1" $ do
     input <- readFile "./test/Day21/input.txt"
     part1 input `shouldBe` "hello_santa"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day21/input.txt"
     part2 input `shouldBe` "hello_santa"
+
+  describe "parseFoods" $ do
+    it "can parse the example" $ do
+      input <- readFile "./test/Day21/example.txt"
+      let exampleFoods =
+            [ Food (Set.fromList ["kfcds", "mxmxvkd", "nhms", "sqjhc"]) (Set.fromList ["dairy", "fish"]),
+              Food (Set.fromList ["fvjkl", "mxmxvkd", "sbzzf", "trh"]) (Set.fromList ["dairy"]),
+              Food (Set.fromList ["fvjkl", "sqjhc"]) (Set.fromList ["soy"]),
+              Food (Set.fromList ["mxmxvkd", "sbzzf", "sqjhc"]) (Set.fromList ["fish"])
+            ]
+      parseFoods input `shouldBe` Right exampleFoods
