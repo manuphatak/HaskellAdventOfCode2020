b300701143fefef51607d730eca3305e86f6d1c8
Manu Phatak <manu@phatak.io>
2021-01-01 20:18:30 -0800

    remove explicit imports from template

--- test/Template/SolutionSpec.hs
+++ test/Template/SolutionSpec.hs
@@ -1,6 +1,6 @@
 module Template.SolutionSpec (spec) where
 
-import Template.Solution (part1, part2)
+import Template.Solution
 import Test.Hspec
 
 spec :: Spec
@@ -8,6 +8,7 @@ spec = parallel $ do
   it "solves Part 1" $ do
     input <- readFile "./test/Template/input.txt"
     part1 input `shouldBe` "hello_santa"
+
   it "solves Part 2" $ do
     input <- readFile "./test/Template/input.txt"
     part2 input `shouldBe` "hello_santa"
