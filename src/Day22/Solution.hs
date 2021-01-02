8aed6d4ca65139ff9b50714d4b559aa4b929ecf2
Manu Phatak <manu@phatak.io>
2021-01-01 21:16:11 -0800

    Solve part 1

--- src/Day22/Solution.hs
+++ src/Day22/Solution.hs
@@ -1,7 +1,38 @@
 module Day22.Solution where
 
+import Advent.Parser
+import Advent.Utils
+import Data.Foldable
+import Data.Function
+import Data.Sequence hiding (zipWith)
+import Text.Parsec hiding (Empty)
+import Prelude hiding (reverse)
+
 part1 :: String -> String
-part1 = head . lines
+part1 = show . winningScore . play . fromRightOrShowError . parseGame
 
 part2 :: String -> String
 part2 = head . lines
+
+data Game = Game (Seq Int) (Seq Int) Int deriving (Show, Eq)
+
+parseGame :: String -> Either ParseError Game
+parseGame = parse gameParser ""
+  where
+    gameParser :: Parsec String () Game
+    gameParser = Game <$> playerParser "1" <*> (newline *> playerParser "2" <* eof) <*> pure 0
+
+    playerParser :: String -> Parsec String () (Seq Int)
+    playerParser n = string ("Player " ++ n ++ ":") *> newline *> (fromList <$> intParser `sepEndBy1` newline)
+
+play :: Game -> Game
+play (Game (x :<| xs) (y :<| ys) i)
+  | x > y = play $ Game (xs |> x |> y) ys (succ i)
+  | y > x = play $ Game xs (ys |> y |> x) (succ i)
+  | otherwise = play $ Game (xs |> x) (ys |> y) (succ i)
+play game = game
+
+winningScore :: Game -> Int
+winningScore (Game Empty scores _) = scores & reverse & toList & zipWith (*) [1 ..] & sum
+winningScore (Game scores Empty _) = scores & reverse & toList & zipWith (*) [1 ..] & sum
+winningScore _ = undefined
--- test/Day22/SolutionSpec.hs
+++ test/Day22/SolutionSpec.hs
@@ -1,14 +1,29 @@
 module Day22.SolutionSpec (spec) where
 
+import Data.Sequence
 import Day22.Solution
 import Test.Hspec
 
 spec :: Spec
-spec = parallel $ do
-  xit "solves Part 1" $ do
+spec = focus . parallel $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day22/input.txt"
-    part1 input `shouldBe` "hello_santa"
+    part1 input `shouldBe` "30197"
 
   xit "solves Part 2" $ do
     input <- readFile "./test/Day22/input.txt"
     part2 input `shouldBe` "hello_santa"
+
+  let exampleGame = Game (fromList [9, 2, 6, 3, 1]) (fromList [5, 8, 4, 7, 10]) 0
+  describe "parseGame" $ do
+    it "parses the example" $ do
+      input <- readFile "./test/Day22/example.txt"
+      parseGame input `shouldBe` Right exampleGame
+  let exampleGameFinal = Game empty (fromList [3, 2, 10, 6, 8, 5, 9, 4, 7, 1]) 29
+  describe "play" $ do
+    it "runs the game until one side loses" $ do
+      play exampleGame `shouldBe` exampleGameFinal
+
+  describe "winningScore" $ do
+    it "calculates the winning score" $ do
+      winningScore exampleGameFinal `shouldBe` 306
