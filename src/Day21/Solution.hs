fb3a96056cf2ca7b2156eb4b8d4cfe266d7f8401
Manu Phatak <manu@phatak.io>
2021-01-01 18:38:26 -0800

    Solve part 1

--- src/Day21/Solution.hs
+++ src/Day21/Solution.hs
@@ -1,19 +1,30 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day21.Solution where
 
+import Advent.Utils
+import Control.Monad
+import Data.Function
+import Data.Functor
+import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import Text.Parsec
 
 part1 :: String -> String
-part1 = head . lines
+part1 = show . ap allergenFreeCount (asFoodAllergenMap . asKnowledgeGroup) . fromRightOrShowError . parseFoods
 
 part2 :: String -> String
 part2 = head . lines
 
-type Ingredients = Set.Set String
+type Ingredient = String
 
-type Allergens = Set.Set String
+type Ingredients = Set.Set Ingredient
 
-data Food = Food Ingredients Allergens deriving (Show, Eq)
+type Allergen = String
+
+type Allergens = Set.Set Allergen
+
+data Food = Food {fIngredients :: Ingredients, fAllergens :: Allergens} deriving (Show, Eq)
 
 parseFoods :: String -> Either ParseError [Food]
 parseFoods = parse (foodParser `sepEndBy1` newline) ""
@@ -26,3 +37,43 @@ parseFoods = parse (foodParser `sepEndBy1` newline) ""
 
     allergensParser :: Parsec String () Allergens
     allergensParser = Set.fromList <$> between (string "(contains ") (char ')') (many1 letter `sepBy` string ", ")
+
+type FoodAllergenKnowledge = Map.Map Allergen Ingredients
+
+asKnowledgeGroup :: [Food] -> FoodAllergenKnowledge
+asKnowledgeGroup =
+  Map.fromListWith Set.intersection . concatMap possibilities
+  where
+    possibilities :: Food -> [(Allergen, Ingredients)]
+    possibilities food = food & fAllergens & Set.toList <&> (,fIngredients food)
+
+type FoodAllergenMap = Map.Map Ingredient Allergen
+
+asFoodAllergenMap :: FoodAllergenKnowledge -> FoodAllergenMap
+asFoodAllergenMap knowledge = head . filter (\m -> Map.size m == Map.size knowledge) . map (invertMap . Map.fromList) . foldr (go . expandPair) [] . Map.toList $ knowledge
+  where
+    go :: [(Allergen, Ingredient)] -> [[(Ingredient, Allergen)]] -> [[(Ingredient, Allergen)]]
+    go ps [] = map (: []) ps
+    go xs ys = [x : y | x <- xs, y <- ys]
+
+    expandPair :: (Allergen, Ingredients) -> [(Allergen, Ingredient)]
+    expandPair (allergen, ingredientSet) = map (allergen,) . Set.toList $ ingredientSet
+
+invertMap :: Ord k => Map.Map a k -> Map.Map k a
+invertMap = Map.foldrWithKey invert Map.empty
+  where
+    invert :: (Ord k) => a -> k -> Map.Map k a -> Map.Map k a
+    invert value key = Map.insert key value
+
+allergenFreeCount :: [Food] -> FoodAllergenMap -> Int
+allergenFreeCount foods allergenMap = map countIngredients ingredientSets & sum
+  where
+    allergenFree :: Ingredients
+    allergenFree = ingredientSets & Set.unions & Set.filter (`Map.notMember` allergenMap)
+
+    ingredientSets :: [Ingredients]
+    ingredientSets = foods & map fIngredients
+
+    countIngredients :: Ingredients -> Int
+
+    countIngredients foodIngredients = Set.intersection foodIngredients allergenFree & Set.size
--- test/Day21/SolutionSpec.hs
+++ test/Day21/SolutionSpec.hs
@@ -1,25 +1,48 @@
 module Day21.SolutionSpec (spec) where
 
+import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import Day21.Solution
 import Test.Hspec
 
 spec :: Spec
-spec = focus . parallel $ do
-  xit "solves Part 1" $ do
+spec = parallel $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day21/input.txt"
-    part1 input `shouldBe` "hello_santa"
+    part1 input `shouldBe` "2556"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day21/input.txt"
     part2 input `shouldBe` "hello_santa"
 
+  let exampleFoods =
+        [ Food (Set.fromList ["kfcds", "mxmxvkd", "nhms", "sqjhc"]) (Set.fromList ["dairy", "fish"]),
+          Food (Set.fromList ["fvjkl", "mxmxvkd", "sbzzf", "trh"]) (Set.fromList ["dairy"]),
+          Food (Set.fromList ["fvjkl", "sqjhc"]) (Set.fromList ["soy"]),
+          Food (Set.fromList ["mxmxvkd", "sbzzf", "sqjhc"]) (Set.fromList ["fish"])
+        ]
   describe "parseFoods" $ do
     it "can parse the example" $ do
       input <- readFile "./test/Day21/example.txt"
-      let exampleFoods =
-            [ Food (Set.fromList ["kfcds", "mxmxvkd", "nhms", "sqjhc"]) (Set.fromList ["dairy", "fish"]),
-              Food (Set.fromList ["fvjkl", "mxmxvkd", "sbzzf", "trh"]) (Set.fromList ["dairy"]),
-              Food (Set.fromList ["fvjkl", "sqjhc"]) (Set.fromList ["soy"]),
-              Food (Set.fromList ["mxmxvkd", "sbzzf", "sqjhc"]) (Set.fromList ["fish"])
-            ]
       parseFoods input `shouldBe` Right exampleFoods
+
+  describe "asKnowledgeGroup" $ do
+    it "is the combined intersection of foods" $ do
+      asKnowledgeGroup exampleFoods
+        `shouldBe` Map.fromList
+          [ ("dairy", Set.fromList ["mxmxvkd"]),
+            ("fish", Set.fromList ["mxmxvkd", "sqjhc"]),
+            ("soy", Set.fromList ["sqjhc", "fvjkl"])
+          ]
+
+  let exampleAllergenMap =
+        Map.fromList
+          [ ("mxmxvkd", "dairy"),
+            ("sqjhc", "fish"),
+            ("fvjkl", "soy")
+          ]
+  describe "asFoodAllergenMap" $ do
+    it "creates a list of possible food allergy mappings" $ do
+      asFoodAllergenMap (asKnowledgeGroup exampleFoods) `shouldBe` exampleAllergenMap
+  describe "allergenFreeCount" $ do
+    it "finds the foods not included in the allergen map" $ do
+      allergenFreeCount exampleFoods exampleAllergenMap `shouldBe` 5
