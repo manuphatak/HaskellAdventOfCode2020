c6343c9afca1af8666d7aa43902527b0ad2a8889
Manu Phatak <manu@phatak.io>
2021-01-17 16:49:17 -0800

    resolve part 1

--- src/Day22/Solution.hs
+++ src/Day22/Solution.hs
@@ -11,65 +11,64 @@ import qualified Data.Set as Set
 import Text.Parsec hiding (Empty)
 
 part1 :: String -> String
-part1 = show . winningScore . combat . fromRightOrShowError . parseGame
+part1 = show . winningScore . uncurry (playGameWith const) . fromRightOrShowError . parseDecks
 
 part2 :: String -> String
-part2 = show . winningScore . fst . recursiveCombat 1 1 Set.empty . fromRightOrShowError . parseGame
+part2 = show . winningScore . uncurry (playGameWith const) . fromRightOrShowError . parseDecks
 
-type Game = (Seq Int, Seq Int)
-
-parseGame :: String -> Either ParseError Game
-parseGame = parse gameParser ""
+parseDecks :: String -> Either ParseError (Deck, Deck)
+parseDecks = parse gameParser ""
   where
-    gameParser :: Parsec String () Game
+    gameParser :: Parsec String () (Deck, Deck)
     gameParser = (,) <$> playerParser "1" <*> (newline *> playerParser "2" <* eof)
 
-    playerParser :: String -> Parsec String () (Seq Int)
-    playerParser n = string ("Player " ++ n ++ ":") *> newline *> (Seq.fromList <$> intParser `sepEndBy1` newline)
+    playerParser :: String -> Parsec String () Deck
+    playerParser n = string ("Player " ++ n ++ ":") *> newline *> deckParser
 
-combat :: Game -> Game
-combat (x :<| xs, y :<| ys)
-  | x > y = combat (xs :|> x :|> y, ys)
-  | y > x = combat (xs, ys :|> y :|> x)
-  | otherwise = combat (xs :|> x, ys :|> y)
-combat g = g
+    deckParser :: Parsec String () Deck
+    deckParser = Seq.fromList <$> intParser `sepEndBy1` newline
 
-type History = Set Game
+type Deck = Seq Int
+
+data Player = Player1 | Player2 deriving (Show, Eq)
+
+playGameWith :: a -> Deck -> Deck -> (Player, Deck)
+playGameWith fn = switch
+  where
+    switch :: Deck -> Deck -> (Player, Deck)
+    switch (x :<| xs) (y :<| ys)
+      | x > y = switch (xs :|> x :|> y) ys
+      | y > x = switch xs (ys :|> y :|> x)
+      | otherwise = switch (xs :|> x) (ys :|> y)
+    switch d Empty = (Player1, d)
+    switch Empty d = (Player2, d)
+
+type History = Set (Deck, Deck)
 
 -- | otherwise = trace ("round " ++ show r ++ " game " ++ show i) $ traceShow game $ go game
-recursiveCombat :: Int -> Int -> History -> Game -> (Game, History)
-recursiveCombat i r history game
-  | game `Set.member` history = ((fst game, Empty), nextHistory)
-  | otherwise = go game
-  where
-    go :: Game -> (Game, History)
-    go (x :<| xs, y :<| ys)
-      | x <= length xs && y <= length ys =
-        recursiveCombat (succ i) 1 nextHistory (xs, ys)
-          & (\(g, h) -> recursiveCombat i (succ r) h (nextGame x xs y ys (gameWinner g)))
-      -- & gameWinner
-      -- & nextGame x xs y ys
-      -- & recursiveCombat i (succ r) nextHistory
-      | otherwise =
-        x `compare` y
-          & nextGame x xs y ys
-          & recursiveCombat i (succ r) nextHistory
-    go g = (g, nextHistory)
+-- recursiveCombat :: Int -> Int -> History -> (Deck, Deck) -> ((Deck, Deck), History)
+-- recursiveCombat i r history game
+--   | game `Set.member` history = ((fst game, Empty), nextHistory)
+--   | otherwise = go game
+--   where
+--     go :: (Deck, Deck) -> ((Deck, Deck), History)
+--     go (x :<| xs, y :<| ys)
+--       | x <= length xs && y <= length ys =
+--         recursiveCombat (succ i) 1 nextHistory (xs, ys)
+--           & (\(g, h) -> recursiveCombat i (succ r) h (nextGame x xs y ys (gameWinner g)))
+--       | otherwise =
+--         x `compare` y
+--           & nextGame x xs y ys
+--           & recursiveCombat i (succ r) nextHistory
+--     go g = (g, nextHistory)
 
-    nextHistory :: Set (Seq Int, Seq Int)
-    nextHistory = Set.insert game history
+--     nextHistory :: Set (Seq Int, Seq Int)
+--     nextHistory = Set.insert game history
 
-    nextGame :: Int -> Seq Int -> Int -> Seq Int -> Ordering -> Game
-    nextGame x xs y ys EQ = (xs :|> x, ys :|> y)
-    nextGame x xs y ys LT = (xs, ys :|> y :|> x)
-    nextGame x xs y ys GT = (xs :|> x :|> y, ys)
+--     nextGame :: Int -> Seq Int -> Int -> Seq Int -> Ordering -> (Deck, Deck)
+--     nextGame x xs y ys EQ = (xs :|> x, ys :|> y)
+--     nextGame x xs y ys LT = (xs, ys :|> y :|> x)
+--     nextGame x xs y ys GT = (xs :|> x :|> y, ys)
 
-gameWinner :: Game -> Ordering
-gameWinner (_, Empty) = GT
-gameWinner (Empty, _) = LT
-gameWinner _ = EQ
-
-winningScore :: Game -> Int
-winningScore (Empty, scores) = scores & Seq.reverse & toList & zipWith (*) [1 ..] & sum
-winningScore (scores, Empty) = scores & Seq.reverse & toList & zipWith (*) [1 ..] & sum
-winningScore _ = undefined
+winningScore :: (a, Deck) -> Int
+winningScore (_, deck) = deck & Seq.reverse & toList & zipWith (*) [1 ..] & sum
--- test/Day22/SolutionSpec.hs
+++ test/Day22/SolutionSpec.hs
@@ -16,39 +16,46 @@ spec = focus . parallel $ do
     -- failed 33957 (too low)
     part2 input `shouldBe` "hello_santa"
 
-  let exampleGame1 = (Seq.fromList [9, 2, 6, 3, 1], Seq.fromList [5, 8, 4, 7, 10])
-  let exampleGame2 = (Seq.fromList [43, 19], Seq.fromList [2, 29, 14])
-  describe "parseGame" $ do
+  let exampleDecks1 = (Seq.fromList [9, 2, 6, 3, 1], Seq.fromList [5, 8, 4, 7, 10])
+  let exampleDecks2 = (Seq.fromList [43, 19], Seq.fromList [2, 29, 14])
+  describe "parseDecks" $ do
     context "given example-1.txt" $ do
       it "parses the example" $ do
         input <- readFile "./test/Day22/example-1.txt"
-        parseGame input `shouldBe` Right exampleGame1
+        parseDecks input `shouldBe` Right exampleDecks1
     context "given example-2.txt" $ do
       it "parses the example" $ do
         input <- readFile "./test/Day22/example-2.txt"
-        parseGame input `shouldBe` Right exampleGame2
-  let exampleGameFinal1 = (Seq.empty, Seq.fromList [3, 2, 10, 6, 8, 5, 9, 4, 7, 1]) :: Game
-  let exampleGameFinalRecursive1 = (Seq.empty, Seq.fromList [7, 5, 6, 2, 4, 1, 10, 8, 9, 3]) :: Game
-  let exampleGameFinal2 = (Seq.fromList [43, 19], Seq.empty) :: Game
-  let exampleGameFinalRecursive2 = (Seq.fromList [43, 19], Seq.empty) :: Game
-  describe "combat" $ do
-    context "given example-1.txt" $ do
-      it "runs the game until one side loses" $ do
-        combat exampleGame1 `shouldBe` exampleGameFinal1
-    xcontext "given example-2.txt" $ do
-      it "runs the game until one side loses" $ do
-        combat exampleGame2 `shouldBe` exampleGameFinal2
-  fdescribe "recursiveCombat" $ do
-    context "given example-1.txt" $ do
-      it "runs the game until one side loses" $ do
-        fst (recursiveCombat 1 1 Set.empty exampleGame1) `shouldBe` exampleGameFinalRecursive1
-    xcontext "given example-2.txt" $ do
-      it "runs the game until one side loses" $ do
-        fst (recursiveCombat 1 1 Set.empty exampleGame2) `shouldBe` exampleGameFinalRecursive2
+        parseDecks input `shouldBe` Right exampleDecks2
+
+  let exampleGameFinal = ((), Seq.fromList [3, 2, 10, 6, 8, 5, 9, 4, 7, 1])
+  -- let exampleGameFinalRecursive1 = (Seq.empty, Seq.fromList [7, 5, 6, 2, 4, 1, 10, 8, 9, 3]) :: Game
+  -- let exampleGameFinal2 = (Seq.fromList [43, 19], Seq.empty) :: Game
+  -- let exampleGameFinalRecursive2 = (Seq.fromList [43, 19], Seq.empty) :: Game
+
+  -- let exampleGameFinal1 = (Seq.empty, Seq.fromList [3, 2, 10, 6, 8, 5, 9, 4, 7, 1]) :: Game
+  -- let exampleGameFinalRecursive1 = (Seq.empty, Seq.fromList [7, 5, 6, 2, 4, 1, 10, 8, 9, 3]) :: Game
+  -- let exampleGameFinal2 = (Seq.fromList [43, 19], Seq.empty) :: Game
+  -- let exampleGameFinalRecursive2 = (Seq.fromList [43, 19], Seq.empty) :: Game
+  -- describe "combat" $ do
+  --   context "given example-1.txt" $ do
+  --     it "runs the game until one side loses" $ do
+  --       combat exampleDecks1 `shouldBe` exampleGameFinal1
+  --   xcontext "given example-2.txt" $ do
+  --     it "runs the game until one side loses" $ do
+  --       combat exampleDecks2 `shouldBe` exampleGameFinal2
+  -- describe "recursiveCombat" $ do
+  --   context "given example-1.txt" $ do
+  --     it "runs the game until one side loses" $ do
+  --       fst (recursiveCombat 1 1 Set.empty exampleDecks1) `shouldBe` exampleGameFinalRecursive1
+  --   xcontext "given example-2.txt" $ do
+  --     it "runs the game until one side loses" $ do
+  --       fst (recursiveCombat 1 1 Set.empty exampleDecks2) `shouldBe` exampleGameFinalRecursive2
 
   describe "winningScore" $ do
     context "given example-1.txt" $ do
       it "calculates the winning score" $ do
-        winningScore exampleGameFinal1 `shouldBe` 306
-      it "calculates the winning score" $ do
-        winningScore exampleGameFinalRecursive1 `shouldBe` 291
+        winningScore exampleGameFinal `shouldBe` 306
+
+-- it "calculates the winning score" $ do
+--   winningScore exampleGameFinalRecursive1 `shouldBe` 291
